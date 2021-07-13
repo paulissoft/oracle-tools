f80e06376ea0813028ddb2aa42a3898d395995ff
gpaulissen <gert.jan.paulissen@gmail.com>
2021-07-13 19:23:56 +0200

    Testing installation.

--- db/src/admin/grant.sql
+++ db/src/admin/grant.sql
@@ -4,18 +4,14 @@ define oracle_tools_username = ORACLE_TOOLS
 
 accept oracle_tools_username prompt "Oracle tools schema [&&oracle_tools_username] ? " default "&&oracle_tools_username"
 
-grant CREATE SESSION -
-,CREATE TABLE -
-,CREATE CLUSTER -
-,CREATE SYNONYM -
-,CREATE VIEW -
-,CREATE SEQUENCE -
-,CREATE PROCEDURE -
-,CREATE TRIGGER -
-,CREATE MATERIALIZED VIEW -
-,CREATE TYPE -
-,CREATE OPERATOR -
-,CREATE INDEXTYPE -
-,CREATE DIMENSION -
-,CREATE JOB -
+grant create job -
+,create materialized view -
+,create procedure -
+,create sequence -
+,create session -
+,create synonym -
+,create table -
+,create trigger -
+,create type -
+,create view -
 to &&oracle_tools_username;
