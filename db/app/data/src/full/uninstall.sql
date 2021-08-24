d8152ef059f2e29989b1c0c8c1aa3aa8f2b627e9
gpaulissen <gert.jan.paulissen@gmail.com>
2021-08-24 14:15:36 +0200

    DATA recreated.

--- db/app/data/src/full/uninstall.sql
+++ db/app/data/src/full/uninstall.sql
@@ -16,39 +16,43 @@
 */
 -- pkg_ddl_util v4
 call dbms_application_info.set_module('uninstall.sql', null);
-/* SQL statement 1 (DROP;ORACLE_TOOLS;FUNCTION;DATA_TIMESTAMP;;;;;;;;2) */
+/* SQL statement 1 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;FUNCTION;DATA_SESSION_USERNAME;;PUBLIC;EXECUTE;NO;2) */
 call dbms_application_info.set_action('SQL statement 1');
-DROP FUNCTION ORACLE_TOOLS.DATA_TIMESTAMP;
-
-/* SQL statement 2 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;FUNCTION;DATA_SESSION_USERNAME;;PUBLIC;EXECUTE;NO;2) */
-call dbms_application_info.set_action('SQL statement 2');
 REVOKE EXECUTE ON "ORACLE_TOOLS"."DATA_SESSION_USERNAME" FROM "PUBLIC";
 
-/* SQL statement 3 (DROP;ORACLE_TOOLS;FUNCTION;DATA_SESSION_USERNAME;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 3');
+/* SQL statement 2 (DROP;ORACLE_TOOLS;FUNCTION;DATA_SESSION_USERNAME;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 2');
 DROP FUNCTION ORACLE_TOOLS.DATA_SESSION_USERNAME;
 
-/* SQL statement 4 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;DATA_API_PKG;;PUBLIC;EXECUTE;NO;2) */
+/* SQL statement 3 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;FUNCTION;DATA_TIMESTAMP;;PUBLIC;EXECUTE;NO;2) */
+call dbms_application_info.set_action('SQL statement 3');
+REVOKE EXECUTE ON "ORACLE_TOOLS"."DATA_TIMESTAMP" FROM "PUBLIC";
+
+/* SQL statement 4 (DROP;ORACLE_TOOLS;FUNCTION;DATA_TIMESTAMP;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 4');
+DROP FUNCTION ORACLE_TOOLS.DATA_TIMESTAMP;
+
+/* SQL statement 5 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;DATA_API_PKG;;PUBLIC;EXECUTE;NO;2) */
+call dbms_application_info.set_action('SQL statement 5');
 REVOKE EXECUTE ON "ORACLE_TOOLS"."DATA_API_PKG" FROM "PUBLIC";
 
-/* SQL statement 5 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;DATA_BR_PKG;;PUBLIC;EXECUTE;NO;2) */
-call dbms_application_info.set_action('SQL statement 5');
+/* SQL statement 6 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;DATA_BR_PKG;;PUBLIC;EXECUTE;NO;2) */
+call dbms_application_info.set_action('SQL statement 6');
 REVOKE EXECUTE ON "ORACLE_TOOLS"."DATA_BR_PKG" FROM "PUBLIC";
 
-/* SQL statement 6 (DROP;ORACLE_TOOLS;PACKAGE_BODY;DATA_API_PKG;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 6');
+/* SQL statement 7 (DROP;ORACLE_TOOLS;PACKAGE_BODY;DATA_API_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 7');
 DROP PACKAGE BODY ORACLE_TOOLS.DATA_API_PKG;
 
-/* SQL statement 7 (DROP;ORACLE_TOOLS;PACKAGE_BODY;DATA_BR_PKG;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 7');
+/* SQL statement 8 (DROP;ORACLE_TOOLS;PACKAGE_BODY;DATA_BR_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 8');
 DROP PACKAGE BODY ORACLE_TOOLS.DATA_BR_PKG;
 
-/* SQL statement 8 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;DATA_API_PKG;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 8');
+/* SQL statement 9 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;DATA_API_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 9');
 DROP PACKAGE ORACLE_TOOLS.DATA_API_PKG;
 
-/* SQL statement 9 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;DATA_BR_PKG;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 9');
+/* SQL statement 10 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;DATA_BR_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 10');
 DROP PACKAGE ORACLE_TOOLS.DATA_BR_PKG;
 
