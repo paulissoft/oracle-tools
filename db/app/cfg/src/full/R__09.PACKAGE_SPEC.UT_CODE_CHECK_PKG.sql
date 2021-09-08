74f0ece8fbdb2518152403bff2930a981f5e20e4
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2021-09-08 15:46:46 +0200

    Update R__09.PACKAGE_SPEC.UT_CODE_CHECK_PKG.sql

--- db/app/cfg/src/full/R__09.PACKAGE_SPEC.UT_CODE_CHECK_PKG.sql
+++ db/app/cfg/src/full/R__09.PACKAGE_SPEC.UT_CODE_CHECK_PKG.sql
@@ -5,5 +5,9 @@ create or replace package ut_code_check_pkg is
 -- Do not defined global public variables but use setters and getters
 l_var varchar2(4 char);
 
+procedure ut_assign(p_str out nocopy varchar2);
+
+procedure ut_reference(p_str in varchar2);
+
 end ut_code_check_pkg;
 /
