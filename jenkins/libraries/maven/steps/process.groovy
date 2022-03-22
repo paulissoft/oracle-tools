c7a3c7db2f4704ae7fc897ebd8933f9358b56f26
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2022-03-22 16:54:06 +0100

    Update process.groovy

--- jenkins/libraries/maven/steps/process.groovy
+++ jenkins/libraries/maven/steps/process.groovy
@@ -8,35 +8,24 @@ void call(app_env){
         stages {
             stage("process") {
                 steps {
-                    env.scm_branch = app_env.scm_branch
+                    assert env.maven != null
                     assert env.scm_branch != null
-                    env.scm_credentials = app_env.scm_credentials
                     assert env.scm_credentials != null
-                    env.scm_url = app_env.scm_url
                     assert env.scm_url != null
-                    env.scm_username = app_env.scm_username
                     assert env.scm_username != null
-                    env.scm_email = app_env.scm_email
                     assert env.scm_email != null
-                    env.conf_dir = app_env.conf_dir
                     assert env.conf_dir != null
-                    env.db = app_env.db
                     assert env.db != null
-                    env.db_credentials = app_env.db_credentials
                     assert env.db_credentials != null
-                    env.db_dir = app_env.db_dir
                     assert env.db_dir != null
-                    env.db_actions = app_env.db_actions
                     assert env.db_actions != null
-                    env.apex_dir = app_env.apex_dir
                     assert env.apex_dir != null
-                    env.apex_actions = app_env.apex_actions
                     assert env.apex_actions != null
 
-                    withCredentials([usernamePassword(credentialsId: env.db_credentials, passwordVariable: 'db_password', usernameVariable: 'db_username')]) {
+                    withCredentials([usernamePassword(credentialsId: app_env.db_credentials, passwordVariable: 'DB_PASSWORD', usernameVariable: 'DB_USERNAME')]) {
                         // Clean before build
                         cleanWs()                
-                        git branch: env.scm_branch, credentialsId: env.scm_credentials, url: env.scm_url
+                        git branch: app_env.scm_branch, credentialsId: app_env.scm_credentials, url: app_env.scm_url
 
                         withMaven(maven: maven,
                                   options: [artifactsPublisher(disabled: true), 
@@ -44,20 +33,20 @@ void call(app_env){
                                             openTasksPublisher(disabled: true)]) {
                             sh('''
 pwd
-db_config_dir=`cd $conf_dir && pwd`
-oracle_tools_dir="$WORKSPACE@script/`ls -rt $WORKSPACE@script | grep -v 'scm-key.txt' | tail -1`"
-echo processing DB actions $db_actions in $db_dir with configuration directory $db_config_dir
-set -- $db_actions
-for profile; do mvn -f $db_dir -Doracle-tools.dir=$oracle_tools_dir -Ddb.config.dir=$db_config_dir -Ddb=$db -Ddb.username=$db_username -Ddb.password=$db_password -P$profile; done
+DB_CONFIG_DIR=`cd ${app_env.conf_dir} && pwd`
+ORACLE_TOOLS_DIR="$WORKSPACE@script/`ls -rt $WORKSPACE@script | grep -v 'scm-key.txt' | tail -1`"
+echo processing DB actions ${app_env.db_actions} in ${app_env.db_dir} with configuration directory $DB_CONFIG_DIR
+set -- ${app_env.db_actions}
+for PROFILE; do mvn -f ${app_env.db_dir} -Doracle-tools.dir=$ORACLE_TOOLS_DIR -Ddb.config.dir=$DB_CONFIG_DIR -Ddb=${app_env.db} -Ddb.username=$DB_USERNAME -Ddb.password=$DB_PASSWORD -P$PROFILE; done
 
-echo processing APEX actions $apex_actions in $apex_dir with configuration directory $db_config_dir
-set -- $apex_actions
-for profile; do mvn -f $apex_dir -Doracle-tools.dir=$oracle_tools_dir -Ddb.config.dir=$db_config_dir -Ddb=$db -Ddb.username=$db_username -Ddb.password=$db_password -P$profile; done
+echo processing APEX actions ${app_env.apex_actions} in ${app_env.apex_dir} with configuration directory $DB_CONFIG_DIR
+set -- ${app_env.apex_actions}
+for PROFILE; do mvn -f ${app_env.apex_dir} -Doracle-tools.dir=$ORACLE_TOOLS_DIR -Ddb.config.dir=$DB_CONFIG_DIR -Ddb=${app_env.db} -Ddb.username=$DB_USERNAME -Ddb.password=$DB_PASSWORD -P$PROFILE; done
 
-git config user.name $scm_username
-git config user.email $scm_email
+git config user.name ${app_env.scm_username}
+git config user.email ${app_env.scm_email}
 git add .
-! git commit -m"Triggered Build: $BUILD_NUMBER" || git push --set-upstream origin $scm_branch
+! git commit -m"Triggered Build: $BUILD_NUMBER" || git push --set-upstream origin ${app_env.scm_branch}
                             ''')
                         }
                     }
