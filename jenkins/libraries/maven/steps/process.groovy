6f9e6c70d4596d626820d00fddfe8635a89fb2eb
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2022-03-22 16:12:05 +0100

    Setting up Jenkins Templating Engine

--- jenkins/libraries/maven/steps/process.groovy
+++ jenkins/libraries/maven/steps/process.groovy
@@ -8,47 +8,41 @@ void call(app_env){
         stages {
             stage("process") {
                 steps {
-                    configFileProvider(
-                        [configFile(fileId: app_env.config_file, variable: 'SETTINGS')]) {
-                        script {
-                            def props = readProperties file: env.SETTINGS // from Pipeline Utility Plugin
+                    env.scm_branch = app_env.scm_branch
+                    assert env.scm_branch != null
+                    env.scm_credentials = app_env.scm_credentials
+                    assert env.scm_credentials != null
+                    env.scm_url = app_env.scm_url
+                    assert env.scm_url != null
+                    env.scm_username = app_env.scm_username
+                    assert env.scm_username != null
+                    env.scm_email = app_env.scm_email
+                    assert env.scm_email != null
+                    env.conf_dir = app_env.conf_dir
+                    assert env.conf_dir != null
+                    env.db = app_env.db
+                    assert env.db != null
+                    env.db_credentials = app_env.db_credentials
+                    assert env.db_credentials != null
+                    env.db_dir = app_env.db_dir
+                    assert env.db_dir != null
+                    env.db_actions = app_env.db_actions
+                    assert env.db_actions != null
+                    env.apex_dir = app_env.apex_dir
+                    assert env.apex_dir != null
+                    env.apex_actions = app_env.apex_actions
+                    assert env.apex_actions != null
 
-                            env.scm_branch = props.scm_branch
-                            assert env.scm_branch != null
-                            env.scm_credentials = props.scm_credentials
-                            assert env.scm_credentials != null
-                            env.scm_url = props.scm_url
-                            assert env.scm_url != null
-                            env.scm_username = props.scm_username
-                            assert env.scm_username != null
-                            env.scm_email = props.scm_email
-                            assert env.scm_email != null
-                            env.conf_dir = props.conf_dir
-                            assert env.conf_dir != null
-                            env.db = props.db
-                            assert env.db != null
-                            env.db_credentials = props.db_credentials
-                            assert env.db_credentials != null
-                            env.db_dir = props.db_dir
-                            assert env.db_dir != null
-                            env.db_actions = props.db_actions
-                            assert env.db_actions != null
-                            env.apex_dir = props.apex_dir
-                            assert env.apex_dir != null
-                            env.apex_actions = props.apex_actions
-                            assert env.apex_actions != null
-                        }
+                    withCredentials([usernamePassword(credentialsId: env.db_credentials, passwordVariable: 'db_password', usernameVariable: 'db_username')]) {
+                        // Clean before build
+                        cleanWs()                
+                        git branch: env.scm_branch, credentialsId: env.scm_credentials, url: env.scm_url
 
-                        withCredentials([usernamePassword(credentialsId: env.db_credentials, passwordVariable: 'db_password', usernameVariable: 'db_username')]) {
-                            // Clean before build
-                            cleanWs()                
-                            git branch: env.scm_branch, credentialsId: env.scm_credentials, url: env.scm_url
-
-                            withMaven(maven: maven,
-														          options: [artifactsPublisher(disabled: true), 
-                                                findbugsPublisher(disabled: true), 
-                                                openTasksPublisher(disabled: true)]) {
-                                sh('''
+                        withMaven(maven: maven,
+                                  options: [artifactsPublisher(disabled: true), 
+                                            findbugsPublisher(disabled: true), 
+                                            openTasksPublisher(disabled: true)]) {
+                            sh('''
 pwd
 db_config_dir=`cd $conf_dir && pwd`
 oracle_tools_dir="$WORKSPACE@script/`ls -rt $WORKSPACE@script | grep -v 'scm-key.txt' | tail -1`"
@@ -64,8 +58,7 @@ git config user.name $scm_username
 git config user.email $scm_email
 git add .
 ! git commit -m"Triggered Build: $BUILD_NUMBER" || git push --set-upstream origin $scm_branch
-                                ''')
-                            }
+                            ''')
                         }
                     }
                 }
--- jenkins/pipeline_config.groovy
+++ jenkins/pipeline_config.groovy
@@ -11,8 +11,26 @@ libraries{
 
 application_environments{
     dev{
-        config_file = 'oracle-tools-config-development'
         maven = 'maven-3'
+        
+        // Oracle tools info
+        scm_branch=development
+        scm_credentials=fd87b3b8-8972-4889-be8d-86342abacb22
+        scm_url=git@github.com:paulissoft/oracle-tools.git
+        scm_username=paulissoft
+        scm_email=paulissoft@gmail.com
+
+        conf_dir=conf/src
+
+        db=docker
+        db_host=host.docker.internal
+        db_credentials=oracle-tools-development
+        db_dir=db/app
+        db_actions=db-info db-install db-generate-ddl-full
+
+        apex_dir=apex/app
+        // no import or export actions for now since there is no sqlplus nor sql installed on the build agent(s)
+        apex_actions=apex-inquiry
     }
 }
 
