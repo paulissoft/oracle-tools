796b2891a6c474983c12374bdef7e4ce4ba1d70e
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2022-03-25 17:31:21 +0100

    Update process.groovy

--- jenkins/libraries/maven/steps/process.groovy
+++ jenkins/libraries/maven/steps/process.groovy
@@ -8,20 +8,34 @@ void call(app_env){
         stages {
             stage("process") {
                 steps {
+                    script {
                         env.maven = app_env.maven
                         assert app_env.maven != null
+                        env.scm_branch = app_env.scm_branch
                         assert app_env.scm_branch != null
+                        env.scm_credentials = app_env.scm_credentials
                         assert app_env.scm_credentials != null
+                        env.scm_url = app_env.scm_url
                         assert app_env.scm_url != null
+                        env.scm_username = app_env.scm_username
                         assert app_env.scm_username != null
+                        env.scm_email = app_env.scm_email
                         assert app_env.scm_email != null
+                        env.conf_dir = app_env.conf_dir
                         assert app_env.conf_dir != null
+                        env.db = app_env.db
                         assert app_env.db != null
+                        env.db_credentials = app_env.db_credentials
                         assert app_env.db_credentials != null
+                        env.db_dir = app_env.db_dir
                         assert app_env.db_dir != null
+                        env.db_actions = app_env.db_actions
                         assert app_env.db_actions != null
+                        env.apex_dir = app_env.apex_dir
                         assert app_env.apex_dir != null
+                        env.apex_actions = app_env.apex_actions
                         assert app_env.apex_actions != null
+                    }
                     
                     withCredentials([usernamePassword(credentialsId: app_env.db_credentials, passwordVariable: 'DB_PASSWORD', usernameVariable: 'DB_USERNAME')]) {
                         // Clean before build
@@ -33,8 +47,9 @@ void call(app_env){
                                             findbugsPublisher(disabled: true), 
                                             openTasksPublisher(disabled: true)]) {
                             sh('''
+set -x
 pwd
-DB_CONFIG_DIR=`cd ${app_env.conf_dir} && pwd`
+DB_CONFIG_DIR=`cd ${conf_dir} && pwd`
 ORACLE_TOOLS_DIR="$WORKSPACE@script/`ls -rt $WORKSPACE@script | grep -v 'scm-key.txt' | tail -1`"
 echo processing DB actions ${app_env.db_actions} in ${app_env.db_dir} with configuration directory $DB_CONFIG_DIR
 set -- ${app_env.db_actions}
