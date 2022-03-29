1f73ffdbe31c3fd833a1b3a519a6560052949356
paulissoft <paulissoft@gmail.com>
2022-03-29 15:36:29 +0000

    Triggered Build: 18

--- apex/app/src/export/application/create_application.sql
+++ apex/app/src/export/application/create_application.sql
@@ -37,7 +37,7 @@ wwv_flow_api.create_flow(
 ,p_public_user=>'APEX_PUBLIC_USER'
 ,p_proxy_server=>nvl(wwv_flow_application_install.get_proxy,'')
 ,p_no_proxy_domains=>nvl(wwv_flow_application_install.get_no_proxy_domains,'')
-,p_flow_version=>'Version 2022-03-29 02:58:08'
+,p_flow_version=>'Version 2022-03-29 03:36:21'
 ,p_flow_status=>'AVAILABLE_W_EDIT_LINK'
 ,p_flow_unavailable_text=>'This application is currently unavailable at this time.'
 ,p_exact_substitutions_only=>'Y'
