74784db99b71b4995252fcfe55035ed391c96291
paulissoft <paulissoft@gmail.com>
2022-09-01 13:13:08 +0000

    Database changes. Triggered Build: 34

--- db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_DISPLAY_DDL_SCHEMA.sql
+++ db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_DISPLAY_DDL_SCHEMA.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW "ORACLE_TOOLS"."V_DISPLAY_DDL_SCHEMA" ("SCHEMA_DDL") BEQUEATH CURRENT_USER AS 
+CREATE OR REPLACE VIEW "ORACLE_TOOLS"."V_DISPLAY_DDL_SCHEMA" ("SCHEMA_DDL")  BEQUEATH CURRENT_USER AS 
   select  value(t) as schema_ddl
 from  table(oracle_tools.pkg_ddl_util.get_display_ddl_schema) t;
 
--- db/app/ddl/src/full/R__18.ORACLE_TOOLS.OBJECT_GRANT.V_MY_FETCH_DDL.sql
+++
@@ -1,4 +0,0 @@
-call dbms_application_info.set_module('R__18.ORACLE_TOOLS.OBJECT_GRANT.V_MY_FETCH_DDL.sql', null);
-call dbms_application_info.set_action('SQL statement 1');
-GRANT SELECT ON "ORACLE_TOOLS"."V_MY_FETCH_DDL" TO PUBLIC;
-
