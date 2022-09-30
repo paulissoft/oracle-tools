f8bfc0788a7a3437f46d712a01c02768f5e85675
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2022-09-30 13:08:46 +0200

    Update setup.sh

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-test -n "$(docker compose ls jenkins | grep running)" || \
+test -n "$(docker-compose ls jenkins | grep running)" || \
     docker-compose down --remove-orphans
 docker-compose build
 docker-compose up -d
