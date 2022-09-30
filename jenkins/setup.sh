79c78e54393fbf177d27b91626e5f93fc52efad9
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2022-09-30 13:34:05 +0200

    Update setup.sh

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -eux
 
-test -n "$(docker-compose ls jenkins | grep running)" || \
+# See https://serverfault.com/questions/789601/check-is-container-service-running-with-docker-compose
+
+service=jenkins-controller
+if [ -z `docker-compose ps -q $service` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q $service)` ]
+then
+    echo "Service $service is not running."
+else
+    echo "Service $service is running: shutting it down."
     docker-compose down --remove-orphans
+fi
+
 docker-compose build
 docker-compose up -d
