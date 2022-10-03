c1b565bc3e961a358bf6344b93cb4b0c401b8e40
Gert-Jan Paulissen <paulissoft@gmail.com>
2022-10-03 14:30:38 +0200

    Jenkins as a service or Docker container

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,15 +1,29 @@
 #!/bin/bash -eux
 
+if [ $# -ge 0 ]
+then
+    docker_compose_file=$1
+else    
+    case $(uname) in
+        Linux)
+            docker_compose_file=docker-compose-jenkins-as-service.yml
+            ;;
+        *)
+            docker_compose_file=docker-compose-jenkins-as-docker-container.yml
+            ;;
+    esac
+fi
+
 # See https://serverfault.com/questions/789601/check-is-container-service-running-with-docker-compose
 
 service=jenkins-controller
-if [ -z `docker-compose ps -q $service` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q $service)` ]
+if [ -z `docker-compose -f $docker_compose_file ps -q $service` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose -f $docker_compose_file ps -q $service)` ]
 then
     echo "Service $service is not running."
 else
     echo "Service $service is running: shutting it down."
-    docker-compose down --remove-orphans
+    docker-compose -f $docker_compose_file down --remove-orphans
 fi
 
-docker-compose build
-docker-compose up -d
+docker-compose -f $docker_compose_file build
+docker-compose -f $docker_compose_file up -d
