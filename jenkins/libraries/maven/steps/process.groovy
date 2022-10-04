77316a56486f52a9be1e35731bf7736d6c87d3a6
Gert-Jan Paulissen <paulissoft@gmail.com>
2022-10-04 11:14:40 +0200

    Update process.groovy

--- jenkins/libraries/maven/steps/process.groovy
+++ jenkins/libraries/maven/steps/process.groovy
@@ -1,120 +1,111 @@
 // -*- mode: groovy; coding: utf-8 -*-
 void call(app_env){
     script {
-        /*
-        -- SCM credentials username and e-mail (pipeline config application environment, pipeline config global or environment variable).
-        */
-        
-        // It must be possible to specify the SCM username and email as environment variables in Jenkins configuration.
-        // https://github.com/paulissoft/oracle-tools/issues/70
-        if ( app_env.scm_username != null ) {
-            env.SCM_USERNAME = app_env.scm_username
-        } else if ( pipelineConfig.scm_username != null ) {
-            env.SCM_USERNAME = pipelineConfig.scm_username
+        def is_empty(String value) {
+            return value == null || value.equals("")
         }
-        assert env.SCM_USERNAME != null : "The pipeline configuration must contain a value for 'scm_username' (in application environment '${app_env.long_name}' or global) or environment variable SCM_USERNAME must be defined"
         
-        if ( app_env.scm_email != null ) {
-            env.SCM_EMAIL = app_env.scm_email
-        } else if ( pipelineConfig.scm_email != null ) {
-            env.SCM_EMAIL = pipelineConfig.scm_email
-        }
-        assert env.SCM_EMAIL != null : "The pipeline configuration must contain a value for 'scm_email' (in application environment '${app_env.long_name}' or global) or environment variable SCM_EMAIL must be defined"
+        def set_env(String key, Boolean mandatory=true, Integer level=3, String default_value=null) {
+            String value = app_env[key]
+            String KEY = key.toUpperCase()
+            
+            if (is_empty(value) && level > 1) {
+                value = pipelineConfig[key]
 
+                if (is_empty(value) && level > 2) {
+                    value = env[KEY]
+                }
+            }
+
+            env[KEY] = ( is_empty(value) ? default_value : value )
+
+            if (mandatory) {
+                String error;
+
+                switch(level) {
+                    case 3:
+                        error = "Either application environment '${app_env.long_name}' variable 'app_env.${key}' or " +
+                            "pipeline configuration variable 'pipelineConfig.${key}' or " +
+                            "environment variable 'env.${KEY}' must be a non-empty string"
+                        break
+                    case 2:
+                        error = "Either application environment '${app_env.long_name}' variable 'app_env.${key}' or " +
+                            "pipeline configuration variable 'pipelineConfig.${key}' must be a non-empty string"
+                        break
+                    case 1:
+                        error = "Application environment '${app_env.long_name}' variable 'app_env.${key}' must be a non-empty string"
+                        break
+                    default:
+                        assert level >= 1 && level <= 3: "Level must be between 1 and 3"
+                }
+                
+                assert !is_empty(value) : error
+            }
+        }
+        
         /*
         -- The SCM Oracle Tools project needed to build the SCM project (pipeline config application environment, pipeline config global or environment variable).
         */
 
-        if ( app_env.scm_url_oracle_tools != null ) {
-            env.SCM_URL_ORACLE_TOOLS = app_env.scm_url_oracle_tools
-        } else if ( pipelineConfig.scm_url_oracle_tools != null ) {
-            env.SCM_URL_ORACLE_TOOLS = pipelineConfig.scm_url_oracle_tools
-        }
-
-        if ( app_env.scm_branch_oracle_tools != null ) {
-            env.SCM_BRANCH_ORACLE_TOOLS = app_env.scm_branch_oracle_tools
-        } else if ( pipelineConfig.scm_branch_oracle_tools != null ) {
-            env.SCM_BRANCH_ORACLE_TOOLS = pipelineConfig.scm_branch_oracle_tools
-        }
-
-        if (env.SCM_URL_ORACLE_TOOLS != null && env.SCM_BRANCH_ORACLE_TOOLS != null) {
-            env.SCM_PROJECT_ORACLE_TOOLS = env.SCM_URL_ORACLE_TOOLS.substring(env.SCM_URL_ORACLE_TOOLS.lastIndexOf("/") + 1).replaceAll("\\.git\$", "")
+        set_env('scm_url_oracle_tools', false)
+        set_env('scm_branch_oracle_tools', !is_empty(env.SCM_URL_ORACLE_TOOLS)) // mandatory if the URL is there
+        if (!is_empty(env.SCM_URL_ORACLE_TOOLS)) {
+            set_env('scm_project_oracle_tools', true, 3, env.SCM_URL_ORACLE_TOOLS.substring(env.SCM_URL_ORACLE_TOOLS.lastIndexOf("/") + 1).replaceAll("\\.git\$", ""))
         }
         
         /*
         -- The SCM (database) configuration project needed to build the SCM project (pipeline config application environment, pipeline config global or environment variable).
         */
 
-        if ( app_env.scm_url_config != null ) {
-            env.SCM_URL_CONFIG = app_env.scm_url_config
-        } else if ( pipelineConfig.scm_url_config != null ) {
-            env.SCM_URL_CONFIG = pipelineConfig.scm_url_config
-        }
-
-        if ( app_env.scm_branch_config != null ) {
-            env.SCM_BRANCH_CONFIG = app_env.scm_branch_config
-        } else if ( pipelineConfig.scm_branch_config != null ) {
-            env.SCM_BRANCH_CONFIG = pipelineConfig.scm_branch_config
-        }
-
-        if ( app_env.scm_credentials_config != null ) {
-            env.SCM_CREDENTIALS_CONFIG = app_env.scm_credentials_config
-        } else if ( pipelineConfig.scm_credentials_config != null ) {
-            env.SCM_CREDENTIALS_CONFIG = pipelineConfig.scm_credentials_config
-        }
-
-        if (env.SCM_URL_CONFIG != null && env.SCM_BRANCH_CONFIG != null) {
-            env.SCM_PROJECT_CONFIG = env.SCM_URL_CONFIG.substring(env.SCM_URL_CONFIG.lastIndexOf("/") + 1).replaceAll("\\.git\$", "")
+        set_env('scm_url_config', false)
+        set_env('scm_branch_config', !is_empty(env.SCM_URL_CONFIG)) // mandatory if the URL is there
+        set_env('scm_credentials_config', false)
+        if (!is_empty(env.SCM_URL_CONFIG)) {
+            set_env('scm_project_config', true, 3, env.SCM_URL_CONFIG.substring(env.SCM_URL_CONFIG.lastIndexOf("/") + 1).replaceAll("\\.git\$", ""))
         }
         
-        /*
-        -- The SCM project to work on (pipeline config application environment).
-        */
-        
-        env.SCM_BRANCH = app_env.scm_branch
-        assert env.SCM_BRANCH != null : "The pipeline configuration must contain a value for 'scm_branch' in application environment '${app_env.long_name}'"
-
-        env.SCM_BRANCH_PREV = ( app_env.scm_branch_prev != null ? app_env.scm_branch_prev : ( app_env.previous != null ? app_env.previous.scm_branch : '' ) )
-
-        env.SCM_CREDENTIALS = ( app_env.scm_credentials != null ? app_env.scm_credentials : pipelineConfig.scm_credentials )
-
-        env.SCM_URL = ( app_env.scm_url != null ? app_env.scm_url : pipelineConfig.scm_url )
-        assert env.SCM_URL != null : "The pipeline configuration must contain a value for 'scm_url' (in application environment '${app_env.long_name}' or global)"
-
-        env.SCM_PROJECT = env.SCM_URL.substring(env.SCM_URL.lastIndexOf("/") + 1).replaceAll("\\.git\$", "")
-
         /*
         -- The configuration directory to work on (pipeline config application environment, pipeline config global).
         */
         
-        env.CONF_DIR = ( app_env.conf_dir != null ? app_env.conf_dir : pipelineConfig.conf_dir )
-        assert env.CONF_DIR != null : "The pipeline configuration must contain a value for 'conf_dir' (in application environment '${app_env.long_name}' or global)"
+        set_env('conf_dir')
 
         /*
         -- The database info to work on (pipeline config application environment, pipeline config global).
         */
         
-        env.DB = app_env.db
-        assert env.DB != null : "The pipeline configuration must contain a value for 'db' in application environment '${app_env.long_name}'"
-
-        env.DB_CREDENTIALS = app_env.db_credentials
-        assert env.DB_CREDENTIALS != null : "The pipeline configuration must contain a value for 'db_credentials' in application environment '${app_env.long_name}'"
-
-        env.DB_DIR = ( app_env.db_dir != null ? app_env.db_dir : pipelineConfig.db_dir )
-        assert env.DB_DIR != null : "The pipeline configuration must contain a value for 'db_dir' (in application environment '${app_env.long_name}' or global)"
-
-        env.DB_ACTIONS = app_env.db_actions
-        assert env.DB_ACTIONS != null : "The pipeline configuration must contain a value for 'db_actions' (in application environment '${app_env.long_name}' or global)"
+        set_env('db', true, 1)
+        set_env('db_credentials', true, 1) // application environment specific
+        set_env('db_dir')
+        set_env('db_actions', true, 2) // application environment or pipeline configuration specific
 
         /*
         -- The APEX info to work on (pipeline config application environment, pipeline config global).
         */
         
-        env.APEX_DIR = ( app_env.apex_dir != null ? app_env.apex_dir : pipelineConfig.apex_dir )
-        assert env.APEX_DIR != null : "The pipeline configuration must contain a value for 'apex_dir' (in application environment '${app_env.long_name}' or global)"
+        set_env('apex_dir')
+        set_env('apex_actions', true, 2) // application environment or pipeline configuration specific
+        
+        /*
+        -- SCM credentials username and e-mail
+        */
+        
+        // It must be possible to specify the SCM username and email as environment variables in Jenkins configuration.
+        // https://github.com/paulissoft/oracle-tools/issues/70
+        Boolean credentials_needed = (env.DB_ACTIONS =~ /\bdb-generate-ddl-full\b/) || (env.APEX_ACTIONS =~ /\bapex-export\b/)
+        
+        set_env('scm_username', credentials_needed)
+        set_env('scm_email', credentials_needed)
 
-        env.APEX_ACTIONS = app_env.apex_actions
-        assert env.APEX_ACTIONS != null : "The pipeline configuration must contain a value for 'apex_actions' in application environment '${app_env.long_name}'"
+        /*
+        -- The SCM project to work on (pipeline config application environment).
+        */
+        
+        set_env('scm_branch', true, 1)
+        set_env('scm_branch_prev', false, 1, ( app_env.previous != null ? app_env.previous.scm_branch : '' ))
+        set_env('scm_credentials', credentials_needed)
+        set_env('scm_url', true)
+        set_env('scm_project', true, 3, env.SCM_URL.substring(env.SCM_URL.lastIndexOf("/") + 1).replaceAll("\\.git\$", ""))
     }
     
     withCredentials([usernamePassword(credentialsId: env.DB_CREDENTIALS, passwordVariable: 'DB_PASSWORD', usernameVariable: 'DB_USERNAME')]) {
@@ -123,16 +114,18 @@ void call(app_env){
 
         // checkout of (optional) configuration project (maybe credentials needed)
         script {
+            def is_empty(String value) {
+                return value == null || value.equals("")
+            }
+            
             // skip checkout if the configuration project is the same as project            
-            if (!(env.SCM_PROJECT_CONFIG == null || env.SCM_PROJECT_CONFIG.equals("")) &&
-                !(env.SCM_PROJECT_CONFIG.equals(env.SCM_PROJECT))) {
-                echo "Checking out configuration project ${env.SCM_URL_CONFIG} to ${env.SCM_PROJECT_CONFIG}" 
+            if (!is_empty(env.SCM_PROJECT_CONFIG) &&
+                !env.SCM_PROJECT_CONFIG.equals(env.SCM_PROJECT)) {
+                echo "Checking out configuration project ${env.SCM_URL_CONFIG} to ${env.SCM_PROJECT_CONFIG} with credentials ${env.SCM_CREDENTIALS_CONFIG}" 
                 dir(env.SCM_PROJECT_CONFIG) {
-                    if (!(env.SCM_CREDENTIALS_CONFIG == null || env.SCM_CREDENTIALS_CONFIG.equals(""))) {
-                        echo "credentials: ${env.SCM_CREDENTIALS_CONFIG}" 
+                    if (!is_empty(env.SCM_CREDENTIALS_CONFIG)) {
                         git url: env.SCM_URL_CONFIG, branch: env.SCM_BRANCH_CONFIG, credentialsId: env.SCM_CREDENTIALS_CONFIG
                     } else {
-                        echo "without credentials" 
                         checkout([
                             $class: 'GitSCM', 
                             branches: [[name: '*/' + env.SCM_BRANCH_CONFIG]], 
@@ -144,17 +137,15 @@ void call(app_env){
                     }
                 }
             }
-        }
         
-        // checkout of (optional) Oracle Tools (no credentials needed)
-        script {
+            // checkout of (optional) Oracle Tools (no credentials needed)
+
             // skip checkout if the Oracle Tools project is the same as the (configuration) project
-            if (!(env.SCM_PROJECT_ORACLE_TOOLS == null || env.SCM_PROJECT_ORACLE_TOOLS.equals("")) &&
-                !(env.SCM_PROJECT_ORACLE_TOOLS.equals(env.SCM_PROJECT)) &&
-                !(env.SCM_PROJECT_CONFIG != null && env.SCM_PROJECT_ORACLE_TOOLS.equals(env.SCM_PROJECT_CONFIG))) {
-                echo "Checking out Oracle Tools project ${env.SCM_URL_ORACLE_TOOLS} to ${env.SCM_PROJECT_ORACLE_TOOLS}"
+            if (!is_empty(env.SCM_PROJECT_ORACLE_TOOLS) &&
+                (is_empty(env.SCM_PROJECT)        || !env.SCM_PROJECT_ORACLE_TOOLS.equals(env.SCM_PROJECT)) &&
+                (is_empty(env.SCM_PROJECT_CONFIG) || !env.SCM_PROJECT_ORACLE_TOOLS.equals(env.SCM_PROJECT_CONFIG))) {
+                echo "Checking out Oracle Tools project ${env.SCM_URL_ORACLE_TOOLS} to ${env.SCM_PROJECT_ORACLE_TOOLS} without credentials"
                 dir(env.SCM_PROJECT_ORACLE_TOOLS) {
-                    echo "without credentials" 
                     checkout([
                         $class: 'GitSCM', 
                         branches: [[name: '*/' + env.SCM_BRANCH_ORACLE_TOOLS]], 
@@ -165,17 +156,14 @@ void call(app_env){
                     ])
                 }
             }
-        }
 
-        // checkout of (mandatory) project to build (maybe credentials needed)
-        script {
-            echo "Checking out build project ${env.SCM_URL} to ${env.SCM_PROJECT}"
+            // checkout of (mandatory) project to build (maybe credentials needed)
+
+            echo "Checking out build project ${env.SCM_URL} to ${env.SCM_PROJECT} with credentials ${env.SCM_CREDENTIALS}" 
             dir(env.SCM_PROJECT) {
-                if (!(env.SCM_CREDENTIALS == null || env.SCM_CREDENTIALS.equals(""))) {
-                    echo "credentials: ${env.SCM_CREDENTIALS}" 
+                if (!is_empty(env.SCM_CREDENTIALS)) {                    
                     git url: env.SCM_URL, branch: env.SCM_BRANCH, credentialsId: env.SCM_CREDENTIALS
                 } else {
-                    echo "without credentials" 
                     checkout([
                         $class: 'GitSCM', 
                         branches: [[name: '*/' + env.SCM_BRANCH]], 
@@ -190,7 +178,7 @@ void call(app_env){
                                     findbugsPublisher(disabled: true), 
                                     openTasksPublisher(disabled: true)]) {
                     sh('chmod +x $WORKSPACE/oracle-tools/jenkins/process.sh')
-                    if (!(env.SCM_CREDENTIALS == null || env.SCM_CREDENTIALS.equals(""))) {
+                    if (!is_empty(env.SCM_CREDENTIALS)) {
                         sshagent([env.SCM_CREDENTIALS]) {
                             sh('$WORKSPACE/oracle-tools/jenkins/process.sh')
                         }
