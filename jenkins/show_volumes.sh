cb7821ba1af05e61048951b33a9f1d29ecee0f9e
Gert-Jan Paulissen <paulissoft@gmail.com>
2022-10-06 18:38:29 +0200

    Enhancing debugging.

--- jenkins/show_volumes.sh
+++ jenkins/show_volumes.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -eu
 
 # all possible volumes used
-for v in maven-local-repository jenkins-agent-workspace jenkins-m2-repository
+for item in \
+    maven-local-repository:/home/jenkins/.m2/repository \
+        jenkins-agent-workspace:/home/jenkins/agent/workspace \
+        jenkins-m2-repository:/home/jenkins/.m2/repository
 do
+    v=$(echo $item | cut -d ':' -f 1)
+    d=$(echo $item | cut -d ':' -f 2)
     if docker volume ls | grep $v 1>/dev/null
     then
-        docker_cmd="docker run --rm -it -v $v:/tmp/volume ghcr.io/paulissoft/pato-jenkins-agent:latest"
-        echo "Docker volume $v has $($docker_cmd find /tmp/volume -print | wc -l) files"
+        docker_cmd="docker run --rm -it -v $v:$d ghcr.io/paulissoft/pato-jenkins-agent:latest"
+        echo "Docker volume $v has $($docker_cmd find $d -print | wc -l) files"
         if printenv DEBUG 1>/dev/null
         then
-            docker_cmd="$docker_cmd find /tmp/volume -ls"
+            docker_cmd="$docker_cmd find $d -ls"
             eval $docker_cmd
             echo ""
             echo "The output above has been created by this command: $docker_cmd"
