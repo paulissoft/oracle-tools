08969e4b0079d86b37d136316dbdc7548ec7e891
Gert-Jan Paulissen <paulissoft@gmail.com>
2022-11-28 20:55:00 +0100

    Update R__10.ORACLE_TOOLS.VIEW.V_MY_FETCH_DDL.sql

--- db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_MY_FETCH_DDL.sql
+++ db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_MY_FETCH_DDL.sql
@@ -4,10 +4,13 @@ CREATE OR REPLACE VIEW "ORACLE_TOOLS"."V_MY_FETCH_DDL" ("DDL#", "DDLTEXT", "ITEM
   ,       t.*
   from    table
           ( oracle_tools.pkg_ddl_util.fetch_ddl
-            ( p_schema => user
-            , p_object_type => null
-            , p_object_names => null
-            , p_object_names_include => null
+            ( p_schema_object_filter =>
+                oracle_tools.t_schema_object_filter
+                ( p_schema => user
+                , p_object_type => null
+                , p_object_names => null
+                , p_object_names_include => null
+                )
             , p_use_schema_export => 1
             , p_schema_object_tab => ( select  oracle_tools.pkg_ddl_util.get_schema_object
                                                ( oracle_tools.t_schema_object_filter
