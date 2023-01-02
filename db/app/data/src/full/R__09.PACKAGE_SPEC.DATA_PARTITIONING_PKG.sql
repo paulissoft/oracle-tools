42581d4a76235e822edf0b0db9a1a4375f5dc095
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-01-02 12:07:27 +0100

    Table partitioning.

--- db/app/data/src/full/R__09.PACKAGE_SPEC.DATA_PARTITIONING_PKG.sql
+++ db/app/data/src/full/R__09.PACKAGE_SPEC.DATA_PARTITIONING_PKG.sql
@@ -6,7 +6,9 @@ A package for operations on (soon to be) partitioned tables.
 
 This includes:
 - DDL to alter a table into a partitioned one
-- drop 
+- Show all partitions of a range partitioned table
+- Find partitions of a range partitioned table
+- Drop old partitions
 
 This package has AUTHID CURRENT_USER so that it can be used by 
 any schema issueing partitioning ddl.
@@ -15,22 +17,41 @@ any schema issueing partitioning ddl.
 
 subtype t_value is varchar2(4000 char);
 
+/**
+
+A subtype for ALL_TAB_PARTITIONS.HIGH_VALUE that is a LONG.
+
+**/
+
 type t_range_rec is record
 ( partition_name all_tab_partitions.partition_name%type
 , partition_position all_tab_partitions.partition_position%type
+, interval all_tab_partitions.interval%type -- YES for anchor partition
 , lwb_incl t_value
 , upb_excl t_value
 );
 
+/**
+
+The record information returned by various pipelined functions.
+
+**/
+
 type t_range_tab is table of t_range_rec;
 
+/**
+
+The table information returned by various pipelined functions.
+
+**/
+
 function alter_table_range_partitioning
-( p_table in varchar2  -- checked by ORACLE_TOOLS.DATA_API_PKG.DBMS_ASSERT$SIMPLE_SQL_NAME()
-, p_partition_by in varchar2 default null
-, p_interval in varchar2 default null
-, p_subpartition_by in varchar2 default null
-, p_partition_clause in varchar2 default null
-, p_online in boolean default true
+( p_table_name in varchar2                    -- checked by ORACLE_TOOLS.DATA_API_PKG.DBMS_ASSERT$SIMPLE_SQL_NAME()
+, p_partition_by in varchar2 default null     -- PARTITION BY <p_partition_by> (if not NULL)
+, p_interval in varchar2 default null         -- INTERVAL <p_interval> (if not NULL)
+, p_subpartition_by in varchar2 default null  -- SUBPARTITION BY <p_subpartition_by> (if not NULL)
+, p_partition_clause in varchar2 default null -- describes how partitions are created
+, p_online in boolean default true            -- ONLINE (if TRUE)
 )
 return varchar2;
 
@@ -43,13 +64,18 @@ The table must be in the USERs schema.
 It returns something like:
 
 ```sql
-ALTER TABLE <p_table> MODIFY [ <p_partition_by> ] [ <p_interval> ] [ <p_subpartition_by> ] [ (<p_partition_clause>) ] [ ONLINE ]
+ALTER TABLE <p_table_name> MODIFY
+[ PARTITION BY <p_partition_by> ]
+[ INTERVAL <p_interval> ]
+[ SUBPARTITION BY <p_subpartition_by> ]
+[ <p_partition_clause> ]
+[ ONLINE ]
 ```
 
 **/
 
 function show_partitions_range
-( p_table in varchar2  -- checked by ORACLE_TOOLS.DATA_API_PKG.DBMS_ASSERT$SIMPLE_SQL_NAME()
+( p_table_name in varchar2  -- checked by ORACLE_TOOLS.DATA_API_PKG.DBMS_ASSERT$SIMPLE_SQL_NAME()
 )
 return t_range_tab
 pipelined;
@@ -58,12 +84,58 @@ pipelined;
  
 Return the partitions and their range as stored in USER_TAB_PARTITIONS.
 
-The high_value in that dictionary view is a long like TIMESTAMP' 2021-08-26 00:00:00'.
+The HIGH_VALUE in that dictionary view is a LONG like TIMESTAMP' 2021-08-26 00:00:00'.
 
 The table must be in the USERs schema.
 
 **/
 
+function find_partitions_range
+( p_table_name in varchar2           -- checked by ORACLE_TOOLS.DATA_API_PKG.DBMS_ASSERT$SIMPLE_SQL_NAME()
+, p_reference_timestamp in timestamp -- the reference timestamp to find the reference partition
+, p_operator in varchar2 default '=' -- '<', '=' or '>'
+)
+return t_range_tab
+pipelined;
+
+/**
+ 
+Find the partitions with respect to the reference timestamp and operator.
+
+First the reference partition must be found where the reference timestamp lies inside the range (lwb_incl <= p_reference_timestamp < upb_excl).
+
+Next if no such a reference partition exists: no partitions will be returned.
+
+Else if such a reference partition exists:
+- for operator '=' we return just the reference partition and we are done
+- for operator '<' we will return all partitions before the reference partition
+- for operator '>' we will return all partitions after the reference partition
+
+The table must be in the USERs schema.
+
+**/
+
+procedure drop_old_partitions 
+( p_table_name in varchar2           -- checked by ORACLE_TOOLS.DATA_API_PKG.DBMS_ASSERT$SIMPLE_SQL_NAME()
+, p_reference_timestamp in timestamp -- the reference timestamp to find the reference partition (that will NOT be dropped)
+);
+
+/**
+ 
+Drop partitions before the partition found by the reference timestamp.
+
+This query will be used to drop the old partitions:
+
+```sql
+select  t.* 
+from    table(oracle_tools.data_partitioning_pkg.find_partitions_range(p_table_name, p_reference_timestamp, '<')) t
+where   t.interval = 'YES'
+```
+
+The table must be in the USERs schema.
+
+**/
+                           
 end data_partitioning_pkg;
 /
 
--- db/app/data/src/full/R__14.PACKAGE_BODY.DATA_PARTITIONING_PKG.sql
+++ db/app/data/src/full/R__14.PACKAGE_BODY.DATA_PARTITIONING_PKG.sql
@@ -1,47 +1,97 @@
 CREATE OR REPLACE PACKAGE BODY "DATA_PARTITIONING_PKG" 
 is
 
+-- PRIVATE routines
+
+$if cfg_pkg.c_debugging $then
+
+procedure print(p_range_rec in t_range_rec)
+is
+begin
+  dbug.print
+  ( dbug."info"
+  , 'partition_name: %s; partition_position: %s; interval: %s; lwb_incl: %s; upb_excl: %s'
+  , p_range_rec.partition_name
+  , p_range_rec.partition_position
+  , p_range_rec.interval
+  , p_range_rec.lwb_incl
+  , p_range_rec.upb_excl
+  );
+end print;  
+
+$end
+
+-- PUBLIC routines
+
 function alter_table_range_partitioning
-( p_table in varchar2  -- checked by ORACLE_TOOLS.DATA_API_PKG.DBMS_ASSERT$SIMPLE_SQL_NAME()
+( p_table_name in varchar2
 , p_partition_by in varchar2
-, p_interval in varchar2 default null -- to undo the interval partitioning
+, p_interval in varchar2
 , p_subpartition_by in varchar2
-, p_partition_clause in varchar2 default null -- when you just want to undo interval partitioning
-, p_online in boolean default true
+, p_partition_clause in varchar2
+, p_online in boolean
 )
 return varchar2
 is
+  l_ddl varchar2(32767 char);
 begin
-  return utl_lms.format_message
-         ( 'ALTER TABLE %s MODIFY %s %s %s %s %s'
-         , oracle_tools.data_api_pkg.dbms_assert$simple_sql_name(p_table, 'table')
-         , p_partition_by
-         , p_interval
-         , p_subpartition_by
-         , case when p_partition_clause is not null then '(' || p_partition_clause || ')' end
-         , case when p_online then 'ONLINE' end
-         );
+$if cfg_pkg.c_debugging $then
+  dbug.enter($$PLSQL_UNIT_OWNER || '.' || $$PLSQL_UNIT || '.ALTER_TABLE_RANGE_PARTITIONING');
+  dbug.print
+  ( dbug."input"
+  , 'p_table_name: %s; p_partition_by: %s; p_interval: %s; p_subpartition_by: %s; p_partition_clause: %s'
+  , p_table_name
+  , p_partition_by
+  , p_interval
+  , p_subpartition_by
+  , p_partition_clause
+  );
+  dbug.print
+  ( dbug."input"
+  , 'p_online: %s'
+  , p_online
+  );
+$end
+
+  l_ddl := utl_lms.format_message
+           ( 'ALTER TABLE %s MODIFY %s %s %s %s %s'
+           , oracle_tools.data_api_pkg.dbms_assert$simple_sql_name(p_table_name, 'table')
+           , case when p_partition_by is not null then 'PARTITION BY ' || p_partition_by end
+           , case when p_interval is not null then 'INTERVAL ' || p_interval end
+           , case when p_subpartition_by is not null then 'SUBPARTITION BY ' || p_subpartition_by end
+           , case when p_partition_clause is not null then '(' || p_partition_clause || ')' end
+           , case when p_online then 'ONLINE' end
+           );
+
+$if cfg_pkg.c_debugging $then
+  dbug.print(dbug."output", 'return: %s', l_ddl);
+  dbug.leave;
+$end
+
+  return l_ddl;           
 end alter_table_range_partitioning;
 
 function show_partitions_range
-( p_table in varchar2  -- checked by ORACLE_TOOLS.DATA_API_PKG.DBMS_ASSERT$SIMPLE_SQL_NAME()
+( p_table_name in varchar2
 )
 return t_range_tab
 pipelined
 is
-  l_table constant user_tab_partitions.table_name%type :=
-    trim('"' from oracle_tools.data_api_pkg.dbms_assert$enquote_name(p_table, 'table'));
+  l_table_name constant user_tab_partitions.table_name%type :=
+    trim('"' from oracle_tools.data_api_pkg.dbms_assert$enquote_name(p_table_name, 'table'));
   l_query constant varchar2(4000 char) :=
     utl_lms.format_message
-    ( q'[
-select  p.high_value
-,       p.partition_name
-,       p.partition_position
-from    user_tab_partitions p
-where   p.table_name = '%s']'
-    , l_table
+    ( q'[select p.high_value, p.partition_name, p.partition_position, p.interval from user_tab_partitions p where p.table_name = '%s']'
+    , l_table_name
     );
+  l_cnt simple_integer := 0;
 begin
+$if cfg_pkg.c_debugging $then
+  dbug.enter($$PLSQL_UNIT_OWNER || '.' || $$PLSQL_UNIT || '.SHOW_PARTITIONS_RANGE');
+  dbug.print(dbug."input", 'p_table_name: %s', p_table_name);
+  dbug.print(dbug."info", 'l_query: %s', l_query);
+$end
+
   for r in
   ( with high_values as
     ( select  dbms_xmlgen.getxmltype(l_query) as xml
@@ -49,7 +99,8 @@ begin
     ), rng as
     ( select  partition_name
       ,       partition_position
-      ,       lag(high_value) over (order by high_value) lwb_incl
+      ,       interval
+      ,       lag(high_value) over (order by partition_position) lwb_incl
       ,       high_value as upb_excl
       from    high_values p
       ,       xmltable
@@ -57,47 +108,173 @@ begin
                 passing p.xml
                 columns partition_name varchar2(128) path '/ROW/PARTITION_NAME'
                 ,       partition_position integer path '/ROW/PARTITION_POSITION'
+                ,       interval varchar2(3) path '/ROW/INTERVAL'
                 ,       high_value varchar2(4000 char) path '/ROW/HIGH_VALUE'
               )
     )
-    select  *
+    select  rng.*
     from    rng
+    order by
+            rng.partition_position 
   )
   loop
     pipe row (r);
+    l_cnt := l_cnt + 1;
   end loop;
+
+$if cfg_pkg.c_debugging $then
+  dbug.print(dbug."info", 'l_cnt: %s', l_cnt);
+  dbug.leave;
+$end
+
+  return; -- essential
 end show_partitions_range;
 
-/*
-create or replace procedure drop_old_partitions 
-                            (p_table_name IN VARCHAR2
-                           , p_threshold IN NUMBER)
-   is
-    
-     cursor tab_interval IS
-      select partition_name
-                  , high_value
-            from user_tab_partitions
-           where table_name = p_table_name
-             and interval = 'YES';
-     
-   begin
-   for part in tab_interval
-   loop
-     execute immediate 
-       'begin
-          if months_between(sysdate
-             , '||part.high_value||') > ' 
-                || p_threshold || 'then
-            execute immediate
-              ''alter table '|| p_table_name ||' drop partition ' 
-            || part.partition_name
-            ||''';
-          end if;
-         end;';
-   end loop;
-   end;
-*/
+function find_partitions_range
+( p_table_name in varchar2
+, p_reference_timestamp in timestamp
+, p_operator in varchar2
+)
+return t_range_tab
+pipelined
+is
+  l_partition_lt_reference_tab t_range_tab := t_range_tab();
+  l_reference_found boolean := false;
+  l_cnt simple_integer := 0;
+begin
+$if cfg_pkg.c_debugging $then
+  dbug.enter($$PLSQL_UNIT_OWNER || '.' || $$PLSQL_UNIT || '.FIND_PARTITIONS_RANGE');
+  dbug.print
+  ( dbug."input"
+  , 'p_table_name: %s; p_reference_timestamp: %s; p_operator: %s'
+  , p_table_name
+  , to_char(p_reference_timestamp, 'yyyy-mm-dd hh24:mi:ss.ff9')
+  , p_operator
+  );
+$end
+
+  <<find_loop>>
+  for r in
+  ( select  t.*
+    from    table(oracle_tools.data_partitioning_pkg.show_partitions_range(p_table_name)) t
+  )
+  loop
+    -- r.lwb_incl and r.upb_excl are something like TIMESTAMP' 2000-01-01 00:00:00'
+    if ( r.lwb_incl is null or to_timestamp(substr(r.lwb_incl, 12, 19), 'yyyy-mm-dd hh24:mi:ss') <= p_reference_timestamp ) and
+       ( r.upb_excl is null or p_reference_timestamp < to_timestamp(substr(r.upb_excl, 12, 19), 'yyyy-mm-dd hh24:mi:ss') )
+    then
+      l_reference_found := true;
+      case p_operator
+        when '='
+        then
+$if cfg_pkg.c_debugging $then
+          print(r);
+$end
+          pipe row (r);
+          l_cnt := l_cnt + 1;
+          exit find_loop;
+          
+        when '<'
+        then
+          for i_idx in 1 .. l_partition_lt_reference_tab.count
+          loop
+$if cfg_pkg.c_debugging $then
+            print(l_partition_lt_reference_tab(i_idx));
+$end
+            pipe row (l_partition_lt_reference_tab(i_idx));
+            l_cnt := l_cnt + 1;
+          end loop;
+          exit find_loop;
+          
+        when '>'
+        then
+          null;
+      end case;
+    elsif p_operator = '<' and not(l_reference_found)
+    then
+      -- save the partitions
+      l_partition_lt_reference_tab.extend(1);
+      l_partition_lt_reference_tab(l_partition_lt_reference_tab.last) := r;    
+    elsif p_operator = '>' and l_reference_found
+    then
+$if cfg_pkg.c_debugging $then
+      print(r);
+$end
+      pipe row (r);
+      l_cnt := l_cnt + 1;
+    end if;
+  end loop find_loop;
+
+$if cfg_pkg.c_debugging $then
+  dbug.print
+  ( dbug."info"
+  , 'l_reference_found: %s; l_cnt: %s'
+  , dbug.cast_to_varchar2(l_reference_found)
+  , l_cnt
+  );
+  dbug.leave;
+$end
+
+  return; -- essential
+end find_partitions_range;
+
+procedure drop_old_partitions 
+( p_table_name in varchar2
+, p_reference_timestamp in timestamp -- the reference timestamp to find the reference partition (that will NOT be dropped)
+)
+is
+  l_table_name constant user_tab_partitions.table_name%type :=
+    oracle_tools.data_api_pkg.dbms_assert$enquote_name(p_table_name, 'table');
+  l_partition_lt_reference_tab t_range_tab := t_range_tab();
+  l_ddl varchar2(32767 char);
+begin
+$if cfg_pkg.c_debugging $then
+  dbug.enter($$PLSQL_UNIT_OWNER || '.' || $$PLSQL_UNIT || '.DROP_OLD_PARTITIONS');
+  dbug.print
+  ( dbug."input"
+  , 'p_table_name: %s; p_reference_timestamp: %s'
+  , p_table_name
+  , to_char(p_reference_timestamp, 'yyyy-mm-dd hh24:mi:ss.ff9')
+  );
+$end
+
+  -- retrieve the partition list before dropping them so we are 100% sure the list won't change while dropping
+  select  t.*
+  bulk collect
+  into    l_partition_lt_reference_tab
+  from    table(oracle_tools.data_partitioning_pkg.find_partitions_range(l_table_name, p_reference_timestamp, '<')) t
+  where   t.interval = 'YES';
+
+  if l_partition_lt_reference_tab.count > 0
+  then
+    for i_idx in l_partition_lt_reference_tab.first .. l_partition_lt_reference_tab.last
+    loop
+      l_ddl := utl_lms.format_message
+               ( 'alter table %s drop partition %s'
+               , l_table_name
+               , oracle_tools.data_api_pkg.dbms_assert$enquote_name
+                 ( l_partition_lt_reference_tab(i_idx).partition_name
+                 , 'partition'
+                 )
+               );
+               
+$if cfg_pkg.c_debugging $then
+      dbug.print(dbug."info", 'l_ddl: %s', l_ddl);
+$end
+
+      execute immediate l_ddl;
+    end loop;
+  end if;
+
+$if cfg_pkg.c_debugging $then
+  dbug.leave;
+exception
+  when others
+  then
+    dbug.leave_on_error;
+    raise;
+$end
+end drop_old_partitions;
 
 end DATA_PARTITIONING_PKG;
 /
