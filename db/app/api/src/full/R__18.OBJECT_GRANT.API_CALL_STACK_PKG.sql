aa05da5e2ad6f628189b5a455b693cfb20a59783
Gert-Jan Paulissen <paulissoft@gmail.com>
2023-01-14 15:51:56 +0100

    Added API_CALL_STACK_PKG

---
+++ db/app/api/src/full/R__18.OBJECT_GRANT.API_CALL_STACK_PKG.sql
@@ -0,0 +1,4 @@
+call dbms_application_info.set_module('R__18.OBJECT_GRANT.API_CALL_STACK_PKG.sql', null);
+call dbms_application_info.set_action('SQL statement 1');
+GRANT EXECUTE ON "API_CALL_STACK_PKG" TO PUBLIC;
+
