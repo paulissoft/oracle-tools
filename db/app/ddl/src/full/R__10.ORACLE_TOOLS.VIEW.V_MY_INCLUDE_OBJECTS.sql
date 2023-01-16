562a43fefeead945b55839b2ac92b1a253a61c32
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-01-16 16:09:33 +0100

    Added view V_MY_INCLUDE_OBJECTS

---
+++ db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_MY_INCLUDE_OBJECTS.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE VIEW "ORACLE_TOOLS"."V_MY_INCLUDE_OBJECTS" ("INCLUDE_OBJECT")  BEQUEATH CURRENT_USER AS 
+select  unique
+        oracle_tools.t_schema_object.id
+        ( p_object_schema => t.object_schema()
+        , p_object_type => t.object_type()
+        , p_object_name => case when t.object_type() in ('CONSTRAINT', 'INDEX', 'REF_CONSTRAINT') then '*' else t.object_name() end
+        , p_base_object_schema => t.base_object_schema()
+        , p_base_object_type => t.base_object_type()
+        , p_base_object_name => t.base_object_name()
+        , p_column_name => case t.object_type() when 'COMMENT' then '*' end
+        , p_grantee => case t.object_type() when 'OBJECT_GRANT' then '*' end
+        , p_privilege => case t.object_type() when 'OBJECT_GRANT' then '*' end
+        , p_grantable => case t.object_type() when 'OBJECT_GRANT' then '*' end
+        ) as include_object
+from    table
+        ( oracle_tools.pkg_schema_object_filter.get_schema_objects
+          ( p_schema => user
+          , p_object_type => null
+          , p_object_names => null
+          , p_object_names_include => null
+          , p_grantor_is_schema => 0
+          , p_exclude_objects => null
+          , p_include_objects => null
+          )
+        ) t;
+
---
+++ db/app/ddl/src/full/R__18.ORACLE_TOOLS.OBJECT_GRANT.V_MY_INCLUDE_OBJECTS.sql
@@ -0,0 +1,4 @@
+call dbms_application_info.set_module('R__18.ORACLE_TOOLS.OBJECT_GRANT.V_MY_INCLUDE_OBJECTS.sql', null);
+call dbms_application_info.set_action('SQL statement 1');
+GRANT SELECT ON "ORACLE_TOOLS"."V_MY_INCLUDE_OBJECTS" TO PUBLIC;
+
