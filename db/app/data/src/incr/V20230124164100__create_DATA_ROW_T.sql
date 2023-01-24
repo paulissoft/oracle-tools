2daab2f87e3b99ed7e3e81682f5127862e286c2a
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-01-24 17:23:19 +0100

    Added DML operation.

--- db/app/data/src/incr/V20230124164100__create_DATA_ROW_T.sql
+++ db/app/data/src/incr/V20230124164100__create_DATA_ROW_T.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE TYPE DATA_ROW_T AS OBJECT (
   table_owner varchar2(128 char)
 , table_name varchar2(128 char)
+, operation varchar2(1 byte) -- (I)nsert/(U)pdate/(D)elete
 , key anydata
 , scn number -- DBMS_FLASHBACK.GET_SYSTEM_CHANGE_NUMBER
 )
