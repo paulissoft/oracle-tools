483576d65abdbda9f5c4cdb396391ac19ddb350a
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-02-03 10:10:02 +0100

    Creating subsystem MSG.

--- db/app/data/src/full/R__18.OBJECT_GRANT.DATA_ROW_NOTIFICATION_PRC.sql
+++
@@ -1,4 +0,0 @@
-call dbms_application_info.set_module('R__18.OBJECT_GRANT.DATA_ROW_NOTIFICATION_PRC.sql', null);
-call dbms_application_info.set_action('SQL statement 1');
-GRANT EXECUTE ON "DATA_ROW_NOTIFICATION_PRC" TO PUBLIC;
-
