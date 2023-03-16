6aec2074675aec7d4f6082c6b5952af88bacba95
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-03-16 17:22:04 +0100

    Update R__14.PACKAGE_BODY.API_TIME_PKG.sql

--- db/app/api/src/full/R__14.PACKAGE_BODY.API_TIME_PKG.sql
+++ db/app/api/src/full/R__14.PACKAGE_BODY.API_TIME_PKG.sql
@@ -73,7 +73,8 @@ function elapsed_time
 )
 return seconds_t
 is
-  l_interval constant timestamp_diff_t := sys_extract_utc(p_end) - sys_extract_utc(p_start);
+  -- do not use SYS_EXTRACT_UTC() since it returns a TIMESTAMP datatype, not TIMESTAMP WITH TIME ZONE 
+  l_interval constant timestamp_diff_t := p_end - p_start;
 begin
   return extract(day from l_interval) * 24 * 60 * 60 +
          extract(hour from l_interval) * 60 * 60 +
@@ -109,6 +110,71 @@ is
 begin
   return to_timestamp_tz(p_val, c_timestamp_format);
 end str2timestamp;
+
+$if oracle_tools.cfg_pkg.c_testing $then
+
+procedure ut_get_timestamp
+is
+  l_ts1 timestamp_t;
+  l_ts2 timestamp_t;
+begin
+  l_ts1 := current_timestamp();
+  l_ts2 := systimestamp();
+  ut.expect(delta(l_ts1, l_ts2), '#1').to_be_less_than(1);
+
+  l_ts2 := l_ts1 at time zone 'UTC';
+  ut.expect(delta(l_ts1, l_ts2), '#2').to_equal(0);
+  
+  l_ts1 := get_timestamp;
+  ut.expect(delta(l_ts1, l_ts2), '#3').to_be_less_than(1);
+
+  l_ts2 := l_ts1 at time zone 'UTC';
+  ut.expect(delta(l_ts1, l_ts2), '#4').to_equal(0);
+end ut_get_timestamp;
+ 
+procedure ut_delta
+is
+  l_ts1 constant timestamp_t := timestamp '2023-03-16 14:37:53.929218 +01:00';
+  l_ts2 constant timestamp_t := timestamp '2023-03-16 14:37:54.012484 +00:00';
+begin
+  ut.expect(delta(l_ts1, l_ts2), 'delta').to_equal(3600.083266);
+end ut_delta;
+
+procedure ut_timestamp2str
+is
+  l_ts timestamp_t := timestamp '1997-01-31 09:26:56.66 +02:00';
+  l_ts_str timestamp_str_t := timestamp2str(l_ts);
+begin
+  ut.expect(l_ts_str, 'timestamp as string').to_equal('1997-01-31T09:26:56.660000Z+02:00');
+
+  l_ts := get_timestamp;
+  ut.expect(timestamp2str(l_ts), q'[now = now at time zone 'UTC']').to_equal(timestamp2str(l_ts at time zone 'UTC'));
+  
+  ut.expect(l_ts_str, 'string => timestamp => string').to_equal(timestamp2str(str2timestamp(l_ts_str)));
+end ut_timestamp2str;
+
+procedure ut_str2timestamp
+is
+  l_ts timestamp_t := str2timestamp('2023-03-16T14:37:53.929218Z+01:00');
+begin
+  ut.expect(extract(YEAR from l_ts), 'YEAR').to_equal(2023);
+  ut.expect(extract(MONTH from l_ts), 'MONTH').to_equal(03);
+  ut.expect(extract(DAY from l_ts), 'DAY').to_equal(16);
+  ut.expect(extract(HOUR from l_ts), 'HOUR').to_equal(14 - 1); -- due to timezone hour of +01
+  ut.expect(extract(MINUTE from l_ts), 'MINUTE').to_equal(37);
+  ut.expect(extract(SECOND from l_ts), 'SECOND').to_equal(53.929218);
+  ut.expect(extract(TIMEZONE_HOUR from l_ts), 'TIMEZONE_HOUR').to_equal(+01);
+  ut.expect(extract(TIMEZONE_MINUTE from l_ts), 'TIMEZONE_MINUTE').to_equal(00);
+
+  ut.expect
+  ( str2timestamp('2023-03-16T16:59:40.423483Z+01:00')
+  , 'same timestamp but different time zones'
+  ).to_equal
+  ( str2timestamp('2023-03-16T15:59:40.423483Z+00:00')
+  );
+end ut_str2timestamp;
+
+$end
 
 end API_TIME_PKG;
 /
