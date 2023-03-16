d646c14c91546ee4f057d82b22fca9c922bfd296
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-03-16 12:44:32 +0100

    Update R__14.PACKAGE_BODY.ADMIN_SCHEDULER_PKG.sql

--- db/app/admin/src/full/R__14.PACKAGE_BODY.ADMIN_SCHEDULER_PKG.sql
+++ db/app/admin/src/full/R__14.PACKAGE_BODY.ADMIN_SCHEDULER_PKG.sql
@@ -8,13 +8,19 @@ procedure stop_job
 is
   l_session_id all_scheduler_running_jobs.session_id%type;
 begin
-  select  r.session_id
-  into    l_session_id
-  from    all_scheduler_jobs j
-          left outer join all_scheduler_running_jobs r -- need not be there
-          on r.owner = j.owner and r.job_name = j.job_name
-  where   j.owner = sys_context('USERENV', 'SESSION_USER')
-  and     j.job_name = p_job_name;
+  begin
+    select  r.session_id
+    into    l_session_id
+    from    all_scheduler_jobs j
+            left outer join all_scheduler_running_jobs r -- need not be there
+            on r.owner = j.owner and r.job_name = j.job_name
+    where   j.owner = sys_context('USERENV', 'SESSION_USER')
+    and     j.job_name = p_job_name;
+  exception
+    when no_data_found
+    then
+      l_session_id := null;
+  end;
   
   dbms_scheduler.stop_job(job_name => sys_context('USERENV', 'SESSION_USER') || '.' || p_job_name, force => p_force, commit_semantics => p_commit_semantics);
 
@@ -33,13 +39,18 @@ procedure drop_job
 is
   l_session_id all_scheduler_running_jobs.session_id%type;
 begin
-  select  r.session_id
-  into    l_session_id
-  from    all_scheduler_jobs j
-          left outer join all_scheduler_running_jobs r -- need not be there
-          on r.owner = j.owner and r.job_name = j.job_name
-  where   j.owner = sys_context('USERENV', 'SESSION_USER')
-  and     j.job_name = p_job_name;
+  begin
+    select  r.session_id
+    into    l_session_id
+    from    all_scheduler_jobs j
+            left outer join all_scheduler_running_jobs r -- need not be there
+            on r.owner = j.owner and r.job_name = j.job_name
+    where   j.owner = sys_context('USERENV', 'SESSION_USER')
+    and     j.job_name = p_job_name;
+  exception
+    when no_data_found
+    then
+      l_session_id := null;
 
   dbms_scheduler.drop_job(job_name => sys_context('USERENV', 'SESSION_USER') || '.' || p_job_name, force => p_force, defer => p_defer, commit_semantics => p_commit_semantics);
 
