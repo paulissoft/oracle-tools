1341f7c3c35cad9caf1c1c00401eee70d265fdc0
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2024-02-12 18:27:10 +0100

    Added org.springframework.boot.autoconfigure.jdbc.DataSourceProperties

--- java/smart-pool-data-source/src/main/java/com/paulissoft/pato/jdbc/DataSourceConfiguration.java
+++ java/smart-pool-data-source/src/main/java/com/paulissoft/pato/jdbc/DataSourceConfiguration.java
@@ -1,36 +1,7 @@
 package com.paulissoft.pato.jdbc;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 
 
-@Getter
-@Setter
-@ToString
-@Configuration
-@Component
-public class DataSourceConfiguration {
-
-    // Spring properties
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
-
-    @Value("${spring.datasource.url}")
-    private String url;
-
-    @Value("${spring.datasource.username}")
-    private String username;
-    
-    @ToString.Exclude
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Value("${spring.datasource.type}")
-    private String type;
-
+public class DataSourceConfiguration extends DataSourceProperties {
 }
