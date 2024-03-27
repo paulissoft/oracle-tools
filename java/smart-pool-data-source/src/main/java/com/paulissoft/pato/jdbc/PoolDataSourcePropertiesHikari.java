f3fde3d58af15147a95ca0078220ded7f56f5547
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2024-03-27 10:41:33 +0100

    Update PoolDataSourcePropertiesHikari.java

--- java/smart-pool-data-source/src/main/java/com/paulissoft/pato/jdbc/PoolDataSourcePropertiesHikari.java
+++ java/smart-pool-data-source/src/main/java/com/paulissoft/pato/jdbc/PoolDataSourcePropertiesHikari.java
@@ -1,41 +1,5 @@
 package com.paulissoft.pato.jdbc;
 
 
-public interface PoolDataSourcePropertiesHikari extends PoolDataSourceProperties, PoolDataSourcePropertiesSettersHikari {
-
-    String getDriverClassName();
-    
-    String getJdbcUrl();
-  
-    String getPoolName();
-
-    int getMaximumPoolSize();
-
-    int getMinimumIdle();
-
-    String getDataSourceClassName();
-
-    boolean isAutoCommit();
-
-    long getConnectionTimeout();
-
-    long getIdleTimeout();
-
-    long getMaxLifetime();
-
-    String getConnectionTestQuery();
-
-    long getInitializationFailTimeout();
-
-    boolean isIsolateInternalQueries();
-
-    boolean isAllowPoolSuspension();
-
-    boolean isReadOnly();
-
-    boolean isRegisterMbeans();
-    
-    long getValidationTimeout();
-
-    long getLeakDetectionThreshold();
+public interface PoolDataSourcePropertiesHikari extends PoolDataSourcePropertiesSettersHikari, PoolDataSourcePropertiesGettersHikari {
 }
