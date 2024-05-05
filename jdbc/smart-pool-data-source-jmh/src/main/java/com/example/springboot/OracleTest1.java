f0a0169d99b99edbee3237a709dc18ec44ce1763
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2024-05-05 18:56:33 +0200

    All tests are there.

---
+++ jdbc/smart-pool-data-source-jmh/src/main/java/com/example/springboot/OracleTest1.java
@@ -0,0 +1,35 @@
+//package com.paulissoft.pato.jdbc;
+
+package com.example.springboot;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+// JMH annotations
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public class OracleTest1 {
+
+    @Benchmark
+    public void connectAllSimple(Blackhole bh,
+                                 BenchmarkState bs) throws SQLException {
+        bs.testList.parallelStream().forEach(idx -> {
+                try (final Connection conn = bs.dataSources[1][1][idx].getConnection()) {
+                    bh.consume(conn.getSchema());
+                } catch (SQLException ex1) {
+                    throw new RuntimeException(ex1.getMessage());
+                }});
+    }
+}
