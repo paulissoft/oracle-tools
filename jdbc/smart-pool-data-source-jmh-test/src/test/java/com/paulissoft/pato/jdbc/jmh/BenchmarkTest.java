fc598a38e07a968b1492475644302bfceaea44a6
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2024-05-12 18:55:17 +0200

    Setting up unit test with JMH.

--- jdbc/smart-pool-data-source-jmh-test/src/test/java/com/paulissoft/pato/jdbc/jmh/BenchmarkTest.java
+++ jdbc/smart-pool-data-source-jmh-test/src/test/java/com/paulissoft/pato/jdbc/jmh/BenchmarkTest.java
@@ -1,21 +1,17 @@
 package com.paulissoft.pato.jdbc.jmh;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 
 import org.junit.Test;
-import org.openjdk.jmh.results.format.ResultFormatType;
-import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-abstract public class AbstractBenchmark extends BenchmarkTestRunner {
+public class BenchmarkTest extends BenchmarkTestRunner {
 
-    public AbstractBenchmark() {
+    public BenchmarkTest() {
     }
 
     @Test
-    public void executeJmhRunner() throws RunnerException {
-        BenchmarkTestRunner.execute(Arrays.asList(this.getClass().getSimpleName()));
+    public void executeAll() throws RunnerException {
+        BenchmarkTestRunner.execute(null); // all tests
     }
 }
