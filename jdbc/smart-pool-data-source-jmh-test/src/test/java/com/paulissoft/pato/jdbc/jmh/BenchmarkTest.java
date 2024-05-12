886f76d1230e9dd7d8c8c06dd221a3c7482b7b9c
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2024-05-12 19:05:54 +0200

    Update BenchmarkTest.java

--- jdbc/smart-pool-data-source-jmh-test/src/test/java/com/paulissoft/pato/jdbc/jmh/BenchmarkTest.java
+++ jdbc/smart-pool-data-source-jmh-test/src/test/java/com/paulissoft/pato/jdbc/jmh/BenchmarkTest.java
@@ -1,10 +1,28 @@
 package com.paulissoft.pato.jdbc.jmh;
 
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotEquals;
+//import static org.junit.jupiter.api.Assertions.assertNull;
+
 //import java.util.Arrays;
 
 import org.junit.Test;
 import org.openjdk.jmh.runner.RunnerException;
+import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ExtendWith(SpringExtension.class)
+//@SpringBootTest
+//@EnableConfigurationProperties({MyDomainDataSourceHikari.class, MyOperatorDataSourceHikari.class})
+@ContextConfiguration(classes={ConfigurationFactory.class, ConfigurationFactoryHikari.class, ConfigurationFactoryOracle.class})
+//@TestPropertySource("classpath:application-test.properties")
 public class BenchmarkTest extends BenchmarkTestRunner {
 
     public BenchmarkTest() {
