9fe6dcf46e5f2c74bc587831a8a9ab27b383682b
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2024-05-12 20:03:56 +0200

    smart-pool-data-source-jmh-test deactivated.

--- jdbc/smart-pool-data-source-jmh-test/src/test/java/com/paulissoft/pato/jdbc/jmh/BenchmarkTest.java
+++ jdbc/smart-pool-data-source-jmh-test/src/test/java/com/paulissoft/pato/jdbc/jmh/BenchmarkTest.java
@@ -4,7 +4,7 @@ package com.paulissoft.pato.jdbc.jmh;
 //import static org.junit.jupiter.api.Assertions.assertNotEquals;
 //import static org.junit.jupiter.api.Assertions.assertNull;
 
-//import java.util.Arrays;
+import java.util.Arrays;
 
 import org.junit.Test;
 import org.openjdk.jmh.runner.RunnerException;
@@ -12,8 +12,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.boot.context.properties.EnableConfigurationProperties;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,15 +21,22 @@ import lombok.extern.slf4j.Slf4j;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class)
 //@EnableConfigurationProperties({MyDomainDataSourceHikari.class, MyOperatorDataSourceHikari.class})
-//@ContextConfiguration(classes={ConfigurationFactory.class, ConfigurationFactoryHikari.class, ConfigurationFactoryOracle.class})
-//@TestPropertySource("classpath:application-test.properties")
+@ContextConfiguration(classes={ConfigurationFactory.class/*, ConfigurationFactoryHikari.class, ConfigurationFactoryOracle.class*/})
+@TestPropertySource("classpath:application-test.properties")
 public class BenchmarkTest extends BenchmarkTestRunner {
 
     public BenchmarkTest() {
     }
 
     @Test
+    public void testFactory() {    
+        final Object cfg = SpringContext.getBean("authDataSourceProperties");
+    }
+    
+    /*
+    @Test
     public void executeAll() throws RunnerException {
         BenchmarkTestRunner.execute(null); // all tests
     }
+    */
 }
