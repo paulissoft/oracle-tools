1364266002a41caa7eda67aa8a9a8101f4dcee6f
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-06-21 12:01:30 +0200

    Update SmartPoolDataSourceHikari.java

--- jdbc/smart-pool-data-source/src/main/java/com/paulissoft/pato/jdbc/SmartPoolDataSourceHikari.java
+++ jdbc/smart-pool-data-source/src/main/java/com/paulissoft/pato/jdbc/SmartPoolDataSourceHikari.java
@@ -50,12 +50,12 @@ public class SmartPoolDataSourceHikari
                                          () -> !isOpen(),
                                          this::getWithPoolName);
         
-        poolDataSourceStatistics = getPoolDataSource().determinePoolDataSourceStatistics(parentPoolDataSourceStatistics);
+        poolDataSourceStatistics = determinePoolDataSourceStatistics(getPoolDataSource(), parentPoolDataSourceStatistics);
 
         final SimplePoolDataSourceHikari poolDataSourceOverflow = getPoolDataSourceOverflow();
 
         if (poolDataSourceOverflow != null) {
-            poolDataSourceStatisticsOverflow = poolDataSourceOverflow.determinePoolDataSourceStatistics(parentPoolDataSourceStatistics);
+            poolDataSourceStatisticsOverflow = determinePoolDataSourceStatistics(poolDataSourceOverflow, parentPoolDataSourceStatistics);
         } else {
             poolDataSourceStatisticsOverflow = null;
         }
