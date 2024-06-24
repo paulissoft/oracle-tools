5341a1f8470b1a81932a0086f0d0382a29957ae3
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-06-24 19:46:52 +0200

    Update R__14.PACKAGE_BODY.ADMIN_SCHEDULER_PKG.sql

--- db/app/admin/src/full/R__14.PACKAGE_BODY.ADMIN_SCHEDULER_PKG.sql
+++ db/app/admin/src/full/R__14.PACKAGE_BODY.ADMIN_SCHEDULER_PKG.sql
@@ -10,32 +10,43 @@ is
   l_owner all_scheduler_running_jobs.owner%type;
   l_session_id all_scheduler_running_jobs.session_id%type;
 begin
-  begin
-    select  r.owner
-    ,       r.session_id
-    into    l_owner
-    ,       l_session_id
-    from    all_scheduler_jobs j
-            left outer join all_scheduler_running_jobs r -- need not be there
-            on r.owner = j.owner and r.job_name = j.job_name
-    where   r.owner = p_owner
-    and     ( sys_context('USERENV', 'SESSION_USER') in ( p_owner, $$PLSQL_UNIT_OWNER ) -- User may kill its own jobs, ADMIN may kill anything
-              or
-              sys_context('USERENV', 'ISDBA') = 'TRUE' -- DBA too
-            ) 
-    and     j.job_name = p_job_name;
-  exception
-    when no_data_found
-    then
-      l_session_id := null;
-  end;
-  
+  select  j.owner
+  ,       r.session_id
+  into    l_owner
+  ,       l_session_id
+  from    all_scheduler_jobs j
+          left outer join all_scheduler_running_jobs r -- need not be there
+          on r.owner = j.owner and r.job_name = j.job_name
+  where   j.owner = p_owner
+  and     ( sys_context('USERENV', 'SESSION_USER') in ( p_owner, $$PLSQL_UNIT_OWNER ) -- User may kill its own jobs, ADMIN may kill anything
+            or
+            sys_context('USERENV', 'ISDBA') = 'TRUE' -- DBA too
+          ) 
+  and     j.job_name = p_job_name;
+
   dbms_scheduler.stop_job(job_name => l_owner || '.' || p_job_name, force => p_force, commit_semantics => p_commit_semantics);
 
   if l_session_id is not null
   then
     admin_system_pkg.kill_session(p_sid => l_session_id);
   end if;
+/*  
+exception
+  when no_data_found
+  then
+    raise_application_error
+    ( -20000
+    , utl_lms.format_message
+      ( 'Could not find job %s.%s for unit owner %s, context SESSION_USER %s and ISDBA %s'
+      , p_owner
+      , p_job_name
+      , $$PLSQL_UNIT_OWNER
+      , sys_context('USERENV', 'SESSION_USER')
+      , sys_context('USERENV', 'ISDBA')
+      )
+    , true
+    );
+*/    
 end stop_job;
 
 procedure drop_job
@@ -49,25 +60,19 @@ is
   l_owner all_scheduler_running_jobs.owner%type;
   l_session_id all_scheduler_running_jobs.session_id%type;
 begin
-  begin
-    select  r.owner
-    ,       r.session_id
-    into    l_owner
-    ,       l_session_id
-    from    all_scheduler_jobs j
-            left outer join all_scheduler_running_jobs r -- need not be there
-            on r.owner = j.owner and r.job_name = j.job_name
-    where   r.owner = p_owner
-    and     ( sys_context('USERENV', 'SESSION_USER') in ( p_owner, $$PLSQL_UNIT_OWNER ) -- User may kill its own jobs, ADMIN may kill anything
-              or
-              sys_context('USERENV', 'ISDBA') = 'TRUE' -- DBA too
-            ) 
-    and     j.job_name = p_job_name;
-  exception
-    when no_data_found
-    then
-      l_session_id := null;
-  end;    
+  select  j.owner
+  ,       r.session_id
+  into    l_owner
+  ,       l_session_id
+  from    all_scheduler_jobs j
+          left outer join all_scheduler_running_jobs r -- need not be there
+          on r.owner = j.owner and r.job_name = j.job_name
+  where   j.owner = p_owner
+  and     ( sys_context('USERENV', 'SESSION_USER') in ( p_owner, $$PLSQL_UNIT_OWNER ) -- User may kill its own jobs, ADMIN may kill anything
+            or
+            sys_context('USERENV', 'ISDBA') = 'TRUE' -- DBA too
+          ) 
+  and     j.job_name = p_job_name;
 
   dbms_scheduler.drop_job(job_name => l_owner || '.' || p_job_name, force => p_force, defer => p_defer, commit_semantics => p_commit_semantics);
 
@@ -75,6 +80,23 @@ begin
   then
     admin_system_pkg.kill_session(p_sid => l_session_id);
   end if;
+/*  
+exception
+  when no_data_found
+  then
+    raise_application_error
+    ( -20000
+    , utl_lms.format_message
+      ( 'Could not find job %s.%s for unit owner %s, context SESSION_USER %s and ISDBA %s'
+      , p_owner
+      , p_job_name
+      , $$PLSQL_UNIT_OWNER
+      , sys_context('USERENV', 'SESSION_USER')
+      , sys_context('USERENV', 'ISDBA')
+      )
+    , true
+    );
+*/    
 end drop_job;
 
 end admin_scheduler_pkg;
