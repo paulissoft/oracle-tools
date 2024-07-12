c82253245ac966a7147ab8c687c154d8ffe205b0
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-07-12 13:43:31 +0200

    Delete OverflowPoolDataSource.java

--- jdbc/smart-pool-data-source/src/main/java/com/paulissoft/pato/jdbc/OverflowPoolDataSource.java
+++
@@ -1,9 +0,0 @@
-package com.paulissoft.pato.jdbc;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import lombok.NonNull;
-
-public interface OverflowPoolDataSource {
-    public Connection getConnection(@NonNull final String username, @NonNull final String password, @NonNull final String schema) throws SQLException;
-}
