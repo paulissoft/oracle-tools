66958fe89ff1d083c2133fd495f075901d0c3b70
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-08-05 17:36:32 +0200

    Update R__14.PACKAGE_BODY.ADMIN_SCHEDULER_PKG.sql

--- db/app/admin/src/full/R__14.PACKAGE_BODY.ADMIN_SCHEDULER_PKG.sql
+++ db/app/admin/src/full/R__14.PACKAGE_BODY.ADMIN_SCHEDULER_PKG.sql
@@ -66,9 +66,38 @@ procedure stop_job
 is
   l_session_id all_scheduler_running_jobs.session_id%type;
 begin
+$if oracle_tools.cfg_pkg.c_debugging $then
+  oracle_tools.dbug.enter($$PLSQL_UNIT_OWNER || '.' || $$PLSQL_UNIT || '.' || 'STOP_JOB');
+  oracle_tools.dbug.print
+  ( oracle_tools.dbug."info"
+  , 'p_job_name: %s; p_force: %s; p_commit_semantics: %s; p_owner: %s'
+  , p_job_name
+  , oracle_tools.dbug.cast_to_varchar2(p_force)
+  , p_commit_semantics
+  , p_owner
+  );
+$end
+
   check_current_user_has_privileges(p_owner);
-  
-  dbms_scheduler.stop_job(job_name => '"' || p_owner || '"."' || p_job_name || '"', force => p_force, commit_semantics => p_commit_semantics);
+
+  begin
+    dbms_scheduler.stop_job(job_name => '"' || p_owner || '"."' || p_job_name || '"', force => p_force, commit_semantics => p_commit_semantics);
+$if oracle_tools.cfg_pkg.c_debugging $then
+  exception
+    when others
+    then
+      for r in
+      ( select  j.state
+        from    all_scheduler_jobs j      
+        where   j.owner = p_owner
+        and     j.job_name = p_job_name
+      )
+      loop
+        oracle_tools.dbug.print(oracle_tools.dbug."warning", 'job: "%s"."%s"; state: "%s"', p_owner, p_job_name, r.state); 
+      end loop;
+      raise;
+$end      
+  end;
 
   begin
     get_session_id(p_owner, p_job_name, l_session_id);
@@ -81,6 +110,15 @@ begin
     when no_data_found
     then null;
   end;  
+
+$if oracle_tools.cfg_pkg.c_debugging $then
+  oracle_tools.dbug.leave;
+exception
+  when others
+  then
+    oracle_tools.dbug.leave_on_error;
+    raise;
+$end  
 end stop_job;
 
 procedure drop_job
@@ -93,9 +131,39 @@ procedure drop_job
 is
   l_session_id all_scheduler_running_jobs.session_id%type;
 begin
+$if oracle_tools.cfg_pkg.c_debugging $then
+  oracle_tools.dbug.enter($$PLSQL_UNIT_OWNER || '.' || $$PLSQL_UNIT || '.' || 'DROP_JOB');
+  oracle_tools.dbug.print
+  ( oracle_tools.dbug."info"
+  , 'p_job_name: %s; p_force: %s; p_defer: %s; p_commit_semantics: %s; p_owner: %s'
+  , p_job_name
+  , oracle_tools.dbug.cast_to_varchar2(p_force)
+  , oracle_tools.dbug.cast_to_varchar2(p_defer)
+  , p_commit_semantics
+  , p_owner
+  );
+$end
+
   check_current_user_has_privileges(p_owner);
-  
-  dbms_scheduler.drop_job(job_name => '"' || p_owner || '"."' || p_job_name || '"', force => p_force, defer => p_defer, commit_semantics => p_commit_semantics);
+
+  begin
+    dbms_scheduler.drop_job(job_name => '"' || p_owner || '"."' || p_job_name || '"', force => p_force, defer => p_defer, commit_semantics => p_commit_semantics);
+$if oracle_tools.cfg_pkg.c_debugging $then
+  exception
+    when others
+    then
+      for r in
+      ( select  j.state
+        from    all_scheduler_jobs j      
+        where   j.owner = p_owner
+        and     j.job_name = p_job_name
+      )
+      loop
+        oracle_tools.dbug.print(oracle_tools.dbug."warning", 'job: "%s"."%s"; state: "%s"', p_owner, p_job_name, r.state); 
+      end loop;
+      raise;
+$end
+  end;
 
   begin
     get_session_id(p_owner, p_job_name, l_session_id);
@@ -108,6 +176,15 @@ begin
     when no_data_found
     then null;
   end;  
+
+$if oracle_tools.cfg_pkg.c_debugging $then
+  oracle_tools.dbug.leave;
+exception
+  when others
+  then
+    oracle_tools.dbug.leave_on_error;
+    raise;
+$end  
 end drop_job;
 
 end admin_scheduler_pkg;
