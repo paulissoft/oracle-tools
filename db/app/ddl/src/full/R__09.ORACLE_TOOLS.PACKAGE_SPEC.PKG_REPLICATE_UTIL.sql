5f06f7725cf7e93f681b293b1d5b381182e721a6
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-09-04 11:56:04 +0200

    Added replicate_view.

--- db/app/ddl/src/full/R__09.ORACLE_TOOLS.PACKAGE_SPEC.PKG_REPLICATE_UTIL.sql
+++ db/app/ddl/src/full/R__09.ORACLE_TOOLS.PACKAGE_SPEC.PKG_REPLICATE_UTIL.sql
@@ -29,7 +29,7 @@ Replicate a table to the schema of the current user.
 
 The table owner must be different (or the database link not empty).
 
-On a local database the target schema (the current logged in user) will (re-create) a synonym on the source table. The synonym name will be the same as the source table name.
+On a local database the target schema (the current logged in user) will (re-)create a synonym on the source table. The synonym name will be the same as the source table name.
 
 On a remote database the target schema (the current logged in user) will (re-)create a table based on the source table and column list:
 - when the target can be modified it will be just that: an identical table but only for the columns specified.
@@ -37,6 +37,32 @@ On a remote database the target schema (the current logged in user) will (re-)cr
 Indexes and constraints will be created if applicable.
 
 When this action has been executed successfully, a view is (re-)created (suffix _V) based on the synonym/materialized view and the column list (plus rowid as row_id).
+
+The values for parameter **p_create_or_replace**:
+- null: target object(s) may or may NOT exist
+- CREATE: target object(s) must NOT exist
+- REPLACE target object(s) must exist
+
+**/
+
+procedure replicate_view
+( p_view_name in varchar2 -- the view name
+, p_view_owner in varchar2 -- the view owner
+, p_column_list in varchar2 -- the columns separated by a comma
+, p_create_or_replace in varchar2 default null -- or CREATE/REPLACE
+, p_db_link in varchar2 default null -- database link: the view may reside on a separate database
+);
+
+/**
+
+Replicate a view to the schema of the current user.
+
+The view owner must be different (or the database link not empty).
+
+On a local database the target schema (the current logged in user) will (re-)create a view based on the source view and column list.
+
+On a remote database the target schema (the current logged in user) will (re-)create a materialized view based on the source view and column list.
+The materialized view will be created based on a prebuilt table.
 
 The values for parameter **p_create_or_replace**:
 - null: target object(s) may or may NOT exist
--- db/app/ddl/src/full/R__14.ORACLE_TOOLS.PACKAGE_BODY.PKG_REPLICATE_UTIL.sql
+++ db/app/ddl/src/full/R__14.ORACLE_TOOLS.PACKAGE_BODY.PKG_REPLICATE_UTIL.sql
@@ -1,5 +1,17 @@
 CREATE OR REPLACE PACKAGE BODY "ORACLE_TOOLS"."PKG_REPLICATE_UTIL" IS
 
+-- LOCAL
+procedure execute_immediate(p_statement in varchar2)
+is
+begin
+  execute immediate p_statement;
+exception
+  when others
+  then
+    raise_application_error(-20000, 'Statement failed: ' || p_statement, true);
+end;  
+
+-- PUBLIC
 procedure replicate_table
 ( p_table_name in varchar2 -- the table name
 , p_table_owner in varchar2 -- the table owner
@@ -46,16 +58,6 @@ is
         )
       );
     end if;
-  end;  
-
-  procedure execute_immediate(p_statement in varchar2)
-  is
-  begin
-    execute immediate p_statement;
-  exception
-    when others
-    then
-      raise_application_error(-20000, 'Statement failed: ' || p_statement, true);
   end;  
 begin
   case
@@ -133,6 +135,118 @@ abc
   -- always create a view based on the synonym
   execute_immediate('CREATE OR REPLACE VIEW "' || l_view_name || '" AS SELECT rowid as row_id,' || l_column_list || ' FROM "' || l_synonym_name || '"' || ' ' || l_table_alias);
 end replicate_table;
+
+procedure replicate_view
+( p_view_name in varchar2 -- the view name
+, p_view_owner in varchar2 -- the view owner
+, p_column_list in varchar2 -- the columns separated by a comma
+, p_create_or_replace in varchar2 default null -- or CREATE/REPLACE
+, p_db_link in varchar2 default null -- database link: the view may reside on a separate database
+)
+is
+  l_view_name all_views.view_name%type;
+  l_view_owner all_views.owner%type;
+  l_column_list varchar2(4000 byte) := null;
+  l_column_tab dbms_sql.varchar2a;
+  l_column_name all_tab_columns.column_name%type;
+  l_found pls_integer;
+  l_view_alias constant varchar2(2 byte) := 'VW';
+
+  procedure check_existence(p_count_expected in pls_integer)
+  is
+    l_count_actual pls_integer;
+  begin
+    select  count(*)
+    into    l_count_actual
+    from    user_objects o
+    where   ( o.object_type = 'VIEW' and o.object_name = l_view_name )
+    ;
+    if l_count_actual = p_count_expected
+    then
+      null;
+    else
+      raise_application_error
+      ( -20000
+      , utl_lms.format_message
+        ( 'Checking existence of view "%s". Expected count: %s; actual count: %s'
+        , l_view_name
+        , to_char(p_count_expected)
+        , to_char(l_count_actual)
+        )
+      );
+    end if;
+  end;  
+begin
+  case
+    when p_db_link is null
+    then
+      -- 'abc' => 'ABC' and '"abc"' => 'abc'
+      l_view_owner := trim('"' from oracle_tools.data_api_pkg.dbms_assert$enquote_name(p_view_owner, 'view owner'));
+      l_view_name := trim('"' from oracle_tools.data_api_pkg.dbms_assert$enquote_name(p_view_name, 'view name'));
+
+      -- check column list
+      if p_column_list = '*'
+      then
+        l_column_list := l_view_alias || '.' || p_column_list;
+      else
+        oracle_tools.pkg_str_util.split
+        ( p_str => p_column_list
+        , p_delimiter => ','
+        , p_str_tab => l_column_tab
+        );
+
+        -- should have count >= 1
+        for i_idx in l_column_tab.first .. l_column_tab.last
+        loop
+          /* '
+abc
+' => 'ABC' and '"abc"' => 'abc'
+          */
+          l_column_name := trim(trim(chr(13) from trim(chr(10) from l_column_tab(i_idx))));
+          l_column_name := trim('"' from oracle_tools.data_api_pkg.dbms_assert$enquote_name(l_column_name, 'column name'));
+
+          begin
+            select  1
+            into    l_found
+            from    all_tab_columns c
+            where   c.owner = l_view_owner
+            and     c.table_name = l_view_name
+            and     c.column_name = l_column_name;
+          exception
+            when no_data_found
+            then
+              raise_application_error
+              ( -20000
+              , utl_lms.format_message
+                ( 'Column "%s"."%s"."%s" not found.'
+                , l_view_owner
+                , l_view_name
+                , l_column_name
+                )
+              , true
+              );
+          end;
+
+          l_column_tab(i_idx) := l_view_alias || '.' || '"' || l_column_name || '"';
+        end loop;
+
+        l_column_list := oracle_tools.pkg_str_util.join(l_column_tab);
+      end if;
+
+      case upper(p_create_or_replace)
+        when 'CREATE'
+        then check_existence(0); -- VIEW SHOULD NOT EXIST
+        when 'REPLACE'
+        then check_existence(1); -- VIEW MUST EXIST
+        else null;
+      end case;
+
+      execute_immediate
+      ( 'CREATE OR REPLACE VIEW "' || l_view_name || '" AS ' ||
+        'SELECT ' || l_column_list || ' FROM "' || l_view_owner || '"."' || l_view_name || '"' || ' ' || l_view_alias
+      );
+  end case;
+end;
 
 end pkg_replicate_util;
 /
