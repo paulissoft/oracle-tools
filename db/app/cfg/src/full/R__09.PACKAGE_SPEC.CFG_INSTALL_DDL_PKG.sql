f5327b7e3e3d98de19ab68681c6af4046fc93e9c
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-09-24 17:36:02 +0200

    Update R__09.PACKAGE_SPEC.CFG_INSTALL_DDL_PKG.sql

--- db/app/cfg/src/full/R__09.PACKAGE_SPEC.CFG_INSTALL_DDL_PKG.sql
+++ db/app/cfg/src/full/R__09.PACKAGE_SPEC.CFG_INSTALL_DDL_PKG.sql
@@ -5,25 +5,45 @@ is
 This package defines functions and procedures used by Flyway increments (or more in general by DDL change operations).
 **/
 
+-- *** column_ddl ***
 -- ORA-01430: column being added already exists in table
 c_column_added_already_exists constant pls_integer := -1430;
 
+-- *** table_ddl **
 -- ORA-00955: name is already used by an existing object
 c_object_created_already_exists constant pls_integer := -955;
+-- ORA-00942: table or view does not exist
+c_object_does_not_exist constant pls_integer := -942;
 
+-- *** constraint_ddl ***
 -- ORA-02264: name already used by an existing constraint
 c_constraint_added_already_exists constant pls_integer := -2264;
-
 -- ORA-02260: table can have only one primary key
 c_table_can_have_only_one_primary_key constant pls_integer := -2260;
-
 -- ORA-02275: such a referential constraint already exists in the table
 c_referential_constraint_already_exists constant pls_integer := -2275;
+-- ORA-23292: The constraint does not exist
+c_constraint_does_not_exist constant pls_integer := -23292;
 
+-- *** index_ddl ***
+-- ORA-01418: specified index does not exist
+c_index_does_not_exist constant pls_integer := -1418;
+
+-- *** trigger_ddl ***
 -- ORA-04081: trigger 'FKNTM_BC_CHARGEPOINT_TARIFF_GROUPS' already exists
 c_trigger_already_exists constant pls_integer := -4081;
+-- ORA-04080: trigger 'BC_CP_CHARGE_PROFILE_SCHED_BI' does not exist
+c_trigger_does_not_exist constant pls_integer := -4080;
 
 type t_ignore_sqlcode_tab is table of pls_integer;
+
+procedure ddl_execution_settings
+( p_ddl_lock_timeout in pls_integer default 60 -- alter session set ddl_lock_timeout = p_ddl_lock_timeout;
+, p_dry_run in boolean default false -- Must commands be execute or just shown via dbms_output?
+);
+/**
+Change DDL execution settings.
+**/
 
 procedure column_ddl
 ( p_operation in varchar2 -- The operation: usually ADD, MODIFY or DROP
@@ -40,18 +60,19 @@ procedure table_ddl
 ( p_operation in varchar2 -- The operation: usually CREATE, ALTER or DROP
 , p_table_name in user_tab_columns.table_name%type -- The table name
 , p_extra in varchar2 default null -- To add after the table name
-, p_ignore_sqlcode_tab in t_ignore_sqlcode_tab default t_ignore_sqlcode_tab(c_object_created_already_exists) -- SQL codes to ignore
+, p_ignore_sqlcode_tab in t_ignore_sqlcode_tab default t_ignore_sqlcode_tab(c_object_created_already_exists, c_object_does_not_exist) -- SQL codes to ignore
 );
 /**
 Issues a p_operation || ' TABLE ' || p_table_name || ' ' || p_extra
 **/
 
 procedure constraint_ddl
-( p_operation in varchar2 -- The operation: usually ADD, MODIFY or DROP
+( p_operation in varchar2 -- The operation: usually ADD, MODIFY, RENAME or DROP
 , p_table_name in user_constraints.table_name%type -- The table name
 , p_constraint_name in user_constraints.constraint_name%type -- The constraint name
+, p_constraint_type in user_constraints.constraint_type%type default null -- The constraint type (used when you RENAME or DROP a constraint containing the wildcard %)
 , p_extra in varchar2 default null -- To add after the constraint name
-, p_ignore_sqlcode_tab in t_ignore_sqlcode_tab default t_ignore_sqlcode_tab(c_constraint_added_already_exists, c_table_can_have_only_one_primary_key, c_referential_constraint_already_exists) -- SQL codes to ignore
+, p_ignore_sqlcode_tab in t_ignore_sqlcode_tab default t_ignore_sqlcode_tab(c_constraint_added_already_exists, c_table_can_have_only_one_primary_key, c_referential_constraint_already_exists, c_constraint_does_not_exist) -- SQL codes to ignore
 );
 /**
 Issues a 'ALTER TABLE ' || p_table_name || ' ' || p_operation || ' CONSTRAINT ' || p_constraint_name || ' ' || p_extra
@@ -74,7 +95,7 @@ procedure index_ddl
 , p_index_name in user_indexes.index_name%type -- The index name
 , p_table_name in user_indexes.table_name%type default null -- The table name
 , p_extra in varchar2 default null -- The extra to add to the DDL statement
-, p_ignore_sqlcode_tab in t_ignore_sqlcode_tab default t_ignore_sqlcode_tab(c_object_created_already_exists) -- SQL codes to ignore
+, p_ignore_sqlcode_tab in t_ignore_sqlcode_tab default t_ignore_sqlcode_tab(c_object_created_already_exists, c_index_does_not_exist) -- SQL codes to ignore
 );
 /**
 Issues:
@@ -88,7 +109,7 @@ procedure trigger_ddl
 , p_trigger_extra in varchar2 default null -- The extra to add after the trigger name
 , p_table_name in user_triggers.table_name%type default null -- The table name
 , p_extra in varchar2 default null -- The extra to add to the DDL statement
-, p_ignore_sqlcode_tab in t_ignore_sqlcode_tab default t_ignore_sqlcode_tab(c_trigger_already_exists) -- SQL codes to ignore
+, p_ignore_sqlcode_tab in t_ignore_sqlcode_tab default t_ignore_sqlcode_tab(c_trigger_already_exists, c_trigger_does_not_exist) -- SQL codes to ignore
 );
 /**
 Issues:
