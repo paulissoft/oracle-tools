d5991bb8e99b1ce76de5c25f32811d797e541781
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-09-25 09:14:42 +0200

    Code/doc enhanced.

--- db/app/admin/src/full/R__09.PACKAGE_SPEC.ADMIN_RECOMPILE_PKG.sql
+++ db/app/admin/src/full/R__09.PACKAGE_SPEC.ADMIN_RECOMPILE_PKG.sql
@@ -6,6 +6,15 @@ This package is a wrapper around SYS.UTL_RECOMP for recompiling invalid objects 
 - normal users can only compile their own schema (the user name is sys_context('USERENV', 'SESSION_USER'))
 **/
 
+function get_schemas
+( p_schema in varchar2 default null -- The schema name. If NULL, all schemas with invalid objects (status <> 'VALID') when you are a DBA, otherwise just your schema when there is at least one invalid object.
+, p_exclude_oracle_maintained in pls_integer default 0 -- Exclude all users whose ALL_USERS.ORACLE_MAINTAINED column equals Y (0 means false)?
+)
+return sys.odcivarchar2list;
+/**
+Return the schemas with invalid objects (status <> 'VALID') that you may see (depending on being DBA or not), sorted by schema name.
+**/
+
 type invalid_object_rec_t is record
 ( owner all_objects.owner%type
 , object_name all_objects.object_name%type
@@ -16,7 +25,7 @@ type invalid_object_rec_t is record
 type invalid_object_tab_t is table of invalid_object_rec_t;
 
 function show
-( p_schema_tab in sys.odcivarchar2list default sys.odcivarchar2list(null) -- The schemas for which to show invalid objects. If NULL, all schemas with invalid objects will be shown when you are a DBA, otherwise just the invalid objects from your schema.
+( p_schema_tab in sys.odcivarchar2list default get_schemas -- The schemas for which to show invalid objects.
 )
 return invalid_object_tab_t
 pipelined;
@@ -30,18 +39,18 @@ procedure recomp_parallel
 );
 
 /**
-A wrapper around UTL_RECOMP.RECOMP_PARALLEL.
+A wrapper around UTL_RECOMP.RECOMP_PARALLEL but with a restriction on the schemas to compile based on being a DBA or not.
 
 Uses DBMS_OUTPUT to display the invocation and the invalid objects thereafter.
 **/
 
 procedure recomp_serial
 ( p_schema in varchar2 default null -- The schema in which to recompile invalid objects. If NULL, all invalid objects in the database are recompiled when you are a DBA, otherwise just your schema.
-, p_exclude_oracle_maintained in boolean default true -- exclude all users whose ALL_USERS.ORACLE_MAINTAINED column equals Y
+, p_exclude_oracle_maintained in boolean default true -- Exclude all users whose ALL_USERS.ORACLE_MAINTAINED column equals Y?
 );
 
 /**
-A wrapper around UTL_RECOMP.RECOMP_SERIAL but may run that in a loop due to p_exclude_oracle_maintained.
+A wrapper around UTL_RECOMP.RECOMP_SERIAL but with a restriction on the schemas to compile based on being a DBA or not plus the flag p_exclude_oracle_maintained.
 
 Uses DBMS_OUTPUT to display the invocation(s) for all schemas matching the input parameters and the invalid objects thereafter.
 **/
--- db/app/admin/src/full/R__14.PACKAGE_BODY.ADMIN_RECOMPILE_PKG.sql
+++ db/app/admin/src/full/R__14.PACKAGE_BODY.ADMIN_RECOMPILE_PKG.sql
@@ -1,53 +1,6 @@
 CREATE OR REPLACE PACKAGE BODY "ADMIN_RECOMPILE_PKG" AS 
 
-function get_schemas
-( p_schema in varchar2
-, p_exclude_oracle_maintained in pls_integer default 0
-)
-return sys.odcivarchar2list
-is
-  l_current_user constant all_users.username%type := sys_context('USERENV', 'SESSION_USER');
-  l_dba constant pls_integer := case when sys_context('USERENV', 'ISDBA') = 'TRUE' or l_current_user = $$PLSQL_UNIT_OWNER then 1 else 0 end;
-  l_schema_tab sys.odcivarchar2list;
-begin
-  select  u.username
-  bulk collect
-  into    l_schema_tab
-  from    all_users u
-  where   ( p_exclude_oracle_maintained = 0 or u.oracle_maintained <> 'Y' )
-  and     ( ( l_dba = 1
-              and
-              u.username in ( nvl(p_schema, u.username), upper(p_schema) )
-            )
-            or
-            ( l_dba = 0
-              and
-              u.username = l_current_user
-              and
-              ( p_schema is null or u.username in ( p_schema, upper(p_schema) ) ) -- a normal user can not compile another schema
-            )
-          )
-  order by
-          username;
-
-/*
-  dbms_output.put_line('p_schema: ' || p_schema);
-  dbms_output.put_line('p_exclude_oracle_maintained: ' || p_exclude_oracle_maintained);
-  dbms_output.put_line('l_current_user: ' || l_current_user);
-  dbms_output.put_line('l_dba: ' || l_dba);  
-  dbms_output.put_line('l_schema_tab.count: ' || l_schema_tab.count);
-  if l_schema_tab.count > 0
-  then
-    for i_idx in l_schema_tab.first .. l_schema_tab.last
-    loop
-      dbms_output.put_line('schema ' || i_idx || ': ' || l_schema_tab(i_idx));
-    end loop;
-  end if;
-*/  
-  
-  return l_schema_tab;
-end get_schemas;
-
+-- LOCAL
 procedure show
 ( p_schema_tab in sys.odcivarchar2list
 )
@@ -69,7 +22,58 @@ begin
   dbms_output.put_line(chr(10));
 end show;
 
--- PUBLIC
+-- GLOBAL
+function get_schemas
+( p_schema in varchar2
+, p_exclude_oracle_maintained in pls_integer
+)
+return sys.odcivarchar2list
+is
+  l_current_user constant all_users.username%type := sys_context('USERENV', 'SESSION_USER');
+  l_dba constant pls_integer := case when sys_context('USERENV', 'ISDBA') = 'TRUE' or l_current_user = $$PLSQL_UNIT_OWNER then 1 else 0 end;
+  l_schema_tab sys.odcivarchar2list;
+begin
+  select  u.username
+  bulk collect
+  into    l_schema_tab
+  from    all_users u   
+  where   ( p_exclude_oracle_maintained = 0 or u.oracle_maintained <> 'Y' )
+  and     ( ( l_dba = 1
+              and
+              u.username in ( nvl(p_schema, u.username), upper(p_schema) )
+            )
+            or
+            ( l_dba = 0
+              and
+              u.username = l_current_user
+              and
+              ( p_schema is null or u.username in ( p_schema, upper(p_schema) ) ) -- a normal user can not compile another schema
+            )
+          )
+  intersect
+  select  o.owner as username
+  from    all_objects o
+  where   o.status <> 'VALID'
+  order by
+          username;
+
+/*
+  dbms_output.put_line('p_schema: ' || p_schema);
+  dbms_output.put_line('p_exclude_oracle_maintained: ' || p_exclude_oracle_maintained);
+  dbms_output.put_line('l_current_user: ' || l_current_user);
+  dbms_output.put_line('l_dba: ' || l_dba);  
+  dbms_output.put_line('l_schema_tab.count: ' || l_schema_tab.count);
+  if l_schema_tab.count > 0
+  then
+    for i_idx in l_schema_tab.first .. l_schema_tab.last
+    loop
+      dbms_output.put_line('schema ' || i_idx || ': ' || l_schema_tab(i_idx));
+    end loop;
+  end if;
+*/  
+  
+  return l_schema_tab;
+end get_schemas;
 
 function show
 ( p_schema_tab in sys.odcivarchar2list
@@ -106,7 +110,7 @@ procedure recomp_parallel
 , p_schema in varchar2
 )
 is
-  l_schema_tab sys.odcivarchar2list := get_schemas(p_schema);
+  l_schema_tab constant sys.odcivarchar2list := get_schemas(p_schema);
 begin
   if l_schema_tab.count = 0
   then
@@ -122,7 +126,7 @@ procedure recomp_serial
 , p_exclude_oracle_maintained in boolean
 )
 is
-  l_schema_tab sys.odcivarchar2list := get_schemas(p_schema, case when p_exclude_oracle_maintained then 1 else 0 end);
+  l_schema_tab constant sys.odcivarchar2list := get_schemas(p_schema, case when p_exclude_oracle_maintained then 1 else 0 end);
 begin
   if l_schema_tab.count = 0
   then
