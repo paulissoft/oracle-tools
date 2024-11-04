fe7c48ec381fb946d2d9553d7f279a334d98e10d
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-11-04 18:33:13 +0100

    Update R__10.ORACLE_TOOLS.VIEW.V_MY_SCHEMA_OBJECTS.sql

--- db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_MY_SCHEMA_OBJECTS.sql
+++ db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_MY_SCHEMA_OBJECTS.sql
@@ -1,4 +1,7 @@
 CREATE OR REPLACE VIEW "ORACLE_TOOLS"."V_MY_SCHEMA_OBJECTS" BEQUEATH CURRENT_USER AS
-SELECT  t.*
-FROM    schema_objects_api.get_schema_objects(null) t;
+select  t.obj
+from    v_all_schema_objects t
+where   t.schema_object_filter_id =
+        (select max(f.id) from oracle_tools.schema_object_filters f where f.session_id = sys_context('USERENV', 'SESSION_ID'))
+and     t.generate_ddl = 1;
 
