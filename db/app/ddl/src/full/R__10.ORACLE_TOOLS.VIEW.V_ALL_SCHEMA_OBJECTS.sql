d2690633750c471b00ec27dde3de240983434358
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-11-05 20:00:09 +0100

    Added V_ALL_SCHEMA_DDLS

--- db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_ALL_SCHEMA_OBJECTS.sql
+++ db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_ALL_SCHEMA_OBJECTS.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW "ORACLE_TOOLS"."V_ALL_SCHEMA_OBJECTS" BEQUEATH CURRENT_USER AS
 SELECT  t.*
 ,       t.obj.id() as obj_id
-,       matches_schema_object_fnc(t.schema_object_filter_id, t.obj) as generate_ddl
-FROM    all_schema_objects t;
+,       oracle_tools.matches_schema_object_fnc(t.schema_object_filter_id, t.obj) as generate_ddl
+,       (select 1 from oracle_tools.v_all_schema_ddls d where d.schema_object_filter_id = t.schema_object_filter_id and d.ddl_obj_id = t.obj.id()) as ddl_generated
+FROM    oracle_tools.all_schema_objects t;
