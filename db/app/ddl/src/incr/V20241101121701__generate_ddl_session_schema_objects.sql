8bb2692a09fd674fb785807a5dd0bcdbd093b664
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-11-08 10:38:21 +0100

    Update V20241101121701__generate_ddl_session_schema_objects.sql

--- db/app/ddl/src/incr/V20241101121701__generate_ddl_session_schema_objects.sql
+++ db/app/ddl/src/incr/V20241101121701__generate_ddl_session_schema_objects.sql
@@ -4,10 +4,11 @@ create table generate_ddl_session_schema_objects
 , seq integer constraint generate_ddl_session_schema_objects$ck$seq check (seq >= 1) -- Primary key #3: Sequence within (session_id, schema_object_filter_id)
 , schema_object_id varchar2(500 byte) constraint generate_ddl_session_schema_objects$ck$schema_object_id check (schema_object_id is not null)
 , created timestamp(6) default sys_extract_utc(systimestamp) constraint generate_ddl_session_schema_objects$ck$created check (created is not null)
-, ddl oracle_tools.t_schema_ddl constraint all_schema_ddls$ck$ddl check (ddl is null or ddl.obj is null or ddl.obj.id = schema_object_id) -- only ddl for this schema object
+, ddl oracle_tools.t_schema_ddl
 , constraint generate_ddl_session_schema_objects$pk primary key (session_id, schema_object_filter_id, seq)
 , constraint generate_ddl_session_schema_objects$uk$1 unique (session_id, schema_object_filter_id, schema_object_id)
 , constraint generate_ddl_session_schema_objects$fk$1 foreign key (schema_object_filter_id, schema_object_id) references schema_object_filter_results(schema_object_filter_id, schema_object_id) on delete cascade
+, constraint all_schema_ddls$ck$1 check (ddl is null or ddl.obj is null or ddl.obj.id = schema_object_id) -- only ddl for this schema object
 )
 nested table ddl.ddl_tab store as generate_ddl_session_schema_objects$ddl$ddl_tab
 ( nested table text store as generate_ddl_session_schema_objects$ddl$ddl_tab$text_tab )
