1d699f219699e3e5c3d69d50ae602df414318ef4
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-11-09 12:06:59 +0100

    Update V20210104170000.04__t_dependent_or_granted_object.sql

--- db/app/ddl/src/incr/V20210104170000.04__t_dependent_or_granted_object.sql
+++ db/app/ddl/src/incr/V20210104170000.04__t_dependent_or_granted_object.sql
@@ -2,15 +2,6 @@ begin
   execute immediate q'[
 create type oracle_tools.t_dependent_or_granted_object authid current_user under oracle_tools.t_schema_object
 ( base_object_id$ varchar2(500 byte)
-  -- begin of constructor(s)
-, constructor function t_dependent_or_granted_object
-  ( self in out nocopy oracle_tools.t_dependent_or_granted_object
-  , network_link$ in varchar2
-  , object_schema$ in varchar2
-  , base_object_id$ in varchar2
-  )
-  return self as result
-/** Constructor without id since that must be determined by the procedure construct below. **/
   -- other methods
 , member function base_object_id return varchar2 deterministic
 , overriding member function base_object_schema return varchar2 deterministic
