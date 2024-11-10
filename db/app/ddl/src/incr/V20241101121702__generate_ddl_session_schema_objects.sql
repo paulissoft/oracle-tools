f54280605b789acdc15c4f740c0ddfcf20c98e8f
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-11-10 10:39:00 +0100

    Update V20241101121702__generate_ddl_session_schema_objects.sql

--- db/app/ddl/src/incr/V20241101121702__generate_ddl_session_schema_objects.sql
+++ db/app/ddl/src/incr/V20241101121702__generate_ddl_session_schema_objects.sql
@@ -1,25 +1,35 @@
 create table generate_ddl_session_schema_objects
-( session_id number not null
+( session_id number not null -- Primary key #1
+, seq integer not null -- Primary key #2: Sequence within (session_id)
+  constraint generate_ddl_session_schema_objects$ck$seq check (seq >= 1) 
 , schema_object_filter_id integer not null
-, seq integer not null
-  constraint generate_ddl_session_schema_objects$ck$seq check (seq >= 1) -- Primary key #3: Sequence within (session_id, schema_object_filter_id)
 , schema_object_id varchar2(500 byte) not null
 , created timestamp(6)
   default sys_extract_utc(systimestamp)
   not null  
 , ddl oracle_tools.t_schema_ddl
 , constraint generate_ddl_session_schema_objects$pk
-  primary key (session_id, schema_object_filter_id, seq)
+  primary key (session_id, seq)
 , constraint generate_ddl_session_schema_objects$uk$1
-  unique (session_id, schema_object_filter_id, schema_object_id)
+  unique (session_id, schema_object_id)
 , constraint generate_ddl_session_schema_objects$fk$1
   foreign key (schema_object_filter_id, schema_object_id)
   references schema_object_filter_results(schema_object_filter_id, schema_object_id) on delete cascade
-, constraint generate_ddl_session_schema_objects$fk$2
-  foreign key (session_id, schema_object_filter_id)
-  references generate_ddl_sessions(session_id, schema_object_filter_id) on delete cascade
+, -- The lookup value GENERATE_DDL_SESSIONS.SCHEMA_OBJECT_FILTER_ID must be equal to GENERATE_DDL_SESSION_SCEHMA_OBJECTS.SCHEMA_OBJECT_FILTER_ID.
+  -- There is no simple way of doing this o SCHEMA_OBJECTS_API wil take care of that.
+  constraint generate_ddl_session_schema_objects$fk$2
+  foreign key (session_id)
+  references generate_ddl_sessions(session_id) on delete cascade
 , constraint all_schema_ddls$ck$1 check (ddl is null or ddl.obj is null or ddl.obj.id = schema_object_id) -- only ddl for this schema object
 )
 nested table ddl.ddl_tab store as generate_ddl_session_schema_objects$ddl$ddl_tab
 ( nested table text store as generate_ddl_session_schema_objects$ddl$ddl_tab$text_tab )
 ;
+
+-- foreign key index generate_ddl_session_schema_objects$fk$1
+create index generate_ddl_session_schema_objects$fk$1
+on generate_ddl_session_schema_objects(schema_object_filter_id, schema_object_id);
+
+-- foreign key index generate_ddl_session_schema_objects$fk$2
+create index generate_ddl_session_schema_objects$fk$2
+on generate_ddl_session_schema_objects(session_id);
