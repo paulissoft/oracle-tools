7a8c8064b94944250a215fe333e05110f6d7ff9a
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-11-10 10:37:46 +0100

    Update R__09.ORACLE_TOOLS.PACKAGE_SPEC.SCHEMA_OBJECTS_API.sql

--- db/app/ddl/src/full/R__09.ORACLE_TOOLS.PACKAGE_SPEC.SCHEMA_OBJECTS_API.sql
+++ db/app/ddl/src/full/R__09.ORACLE_TOOLS.PACKAGE_SPEC.SCHEMA_OBJECTS_API.sql
@@ -8,54 +8,61 @@ type t_schema_object_rec is record
 );
 
 type t_schema_object_cursor is ref cursor return t_schema_object_rec;
+
+subtype t_session_id is generate_ddl_session_schema_objects.session_id%type;
    
-function get_last_schema_object_filter_id
-return positiven;
+procedure set_session_id
+( p_session_id in t_session_id
+);
+/** Set the session id for saving/retrieving on GENERATE_DDL_SESSIONS and GENERATE_DDL_SESSION_SCHEMA_OBJECTS. **/
+
+function get_session_id
+return t_session_id;
+/** Get the session id for saving/retrieving on GENERATE_DDL_SESSIONS and GENERATE_DDL_SESSION_SCHEMA_OBJECTS. **/
 
 procedure add
 ( p_schema_object_filter in oracle_tools.t_schema_object_filter -- the schema object filter
 , p_add_schema_objects in boolean default true -- create records for table GENERATE_DDL_SESSION_SCHEMA_OBJECTS (and its parent tables SCHEMA_OBJECTS and SCHEMA_OBJECT_FILTER_RESULTS)?
-, p_schema_object_filter_id in out nocopy positiven -- IN because of positiven but it is in reality an OUT parameter
+, p_session_id in t_session_id default get_session_id
 );
 /** Add a record to table GENERATE_DDL_SESSIONS (and its parent SCHEMA_OBJECT_FILTERS). **/
 
 procedure add
 ( p_schema_object in oracle_tools.t_schema_object -- The schema object to add to GENERATE_DDL_SESSION_SCHEMA_OBJECTS
-, p_schema_object_filter_id in positiven default get_last_schema_object_filter_id
+, p_session_id in t_session_id default get_session_id
 , p_ignore_dup_val_on_index in boolean default false
 );
 /** Add a schema object to GENERATE_DDL_SESSION_SCHEMA_OBJECTS, meaning INSERT, UPDATE OR UPSERT. */
 
 procedure add
 ( p_schema_object_cursor in t_schema_object_cursor -- The schema objects to add to GENERATE_DDL_SESSION_SCHEMA_OBJECTS
-, p_schema_object_filter_id in positiven default get_last_schema_object_filter_id
+, p_session_id in t_session_id default get_session_id
 , p_ignore_dup_val_on_index in boolean default false
 );
 /** Add schema objects to GENERATE_DDL_SESSION_SCHEMA_OBJECTS, meaning INSERT, UPDATE OR UPSERT. */
 
 procedure add
 ( p_schema_ddl in oracle_tools.t_schema_ddl
-, p_schema_object_filter_id in positiven
+, p_session_id in t_session_id default get_session_id
 );
 /** Update the record in table GENERATE_DDL_SESSION_SCHEMA_OBJECTS. **/
 
 function find_schema_object_by_seq
 ( p_seq in integer default 1 -- Find schema object in GENERATE_DDL_SESSION_SCHEMA_OBJECTS by (schema_object_filter_id, seq)
-, p_schema_object_filter_id in positiven default get_last_schema_object_filter_id
+, p_session_id in t_session_id default get_session_id
 )
 return generate_ddl_session_schema_objects%rowtype;
 /** Find the schema object in GENERATE_DDL_SESSION_SCHEMA_OBJECTS by seq. **/
 
 function find_schema_object_by_object_id
 ( p_schema_object_id in varchar2 -- Find schema object in GENERATE_DDL_SESSION_SCHEMA_OBJECTS by (schema_object_filter_id, obj.id())
-, p_schema_object_filter_id in positiven default get_last_schema_object_filter_id
+, p_session_id in t_session_id default get_session_id
 )
 return generate_ddl_session_schema_objects%rowtype;
 /** Find the schema object in GENERATE_DDL_SESSION_SCHEMA_OBJECTS by obj.id(). **/
 
 function get_named_objects
 ( p_schema in varchar2
-, p_schema_object_filter_id in positiven
 )
 return oracle_tools.t_schema_object_tab
 pipelined;
@@ -82,7 +89,7 @@ procedure default_match_perc_threshold
 );
 
 function match_perc
-( p_schema_object_filter_id in positiven default get_last_schema_object_filter_id
+( p_session_id in t_session_id default get_session_id
 )
 return integer
 deterministic;
