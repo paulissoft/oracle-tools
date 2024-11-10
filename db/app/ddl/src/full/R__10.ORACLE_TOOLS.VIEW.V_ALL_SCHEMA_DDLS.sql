d82c2d50a724443f332b152bce94405ebb9a5e59
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-11-10 10:37:52 +0100

    Delete R__10.ORACLE_TOOLS.VIEW.V_ALL_SCHEMA_DDLS.sql

--- db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_ALL_SCHEMA_DDLS.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE VIEW "ORACLE_TOOLS"."V_ALL_SCHEMA_DDLS" BEQUEATH CURRENT_USER AS
-SELECT  t.*
-,       t.ddl.obj.id as ddl_obj_id
-FROM    oracle_tools.generate_ddl_session_schema_objects t;
