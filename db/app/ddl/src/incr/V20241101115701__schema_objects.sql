3ff794b966abfce64fd25d32822b38cd7d338c76
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-11-12 14:15:04 +0100

    Update V20241101115701__schema_objects.sql

--- db/app/ddl/src/incr/V20241101115701__schema_objects.sql
+++ db/app/ddl/src/incr/V20241101115701__schema_objects.sql
@@ -1,1 +1,14 @@
-create table schema_objects of t_schema_object (id primary key) object identifier is primary key;
+create table schema_objects
+( id varchar2(500) not null constraint schema_objects$pk primary key
+, obj t_schema_object 
+, created timestamp(6)
+  default sys_extract_utc(systimestamp)
+  not null
+, constraint schema_objects$ck$obj check (obj is not null and obj.id = id)
+);
+
+create index schema_objects$idx$1
+on schema_objects (obj.object_schema(), obj.object_name(), obj.object_type());
+
+create index schema_objects$idx$2
+on schema_objects (obj.base_object_schema(), obj.base_object_name(), obj.base_object_type());
