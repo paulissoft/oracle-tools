8526d5c4c729f1f87123f9216b164887c3d7ebbb
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-11-13 18:11:57 +0100

    Update R__10.ORACLE_TOOLS.VIEW.V_MY_NAMED_SCHEMA_OBJECTS.sql

--- db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_MY_NAMED_SCHEMA_OBJECTS.sql
+++ db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_MY_NAMED_SCHEMA_OBJECTS.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE FORCE /* v_my_schema_objects will be created later */ VIEW "ORACLE_TOOLS"."V_MY_NAMED_SCHEMA_OBJECTS" BEQUEATH CURRENT_USER AS
-select  treat(t.obj as oracle_tools.t_named_object) as obj
+CREATE OR REPLACE FORCE /* v_my_schema_objects will be created later */ VIEW "ORACLE_TOOLS"."V_MY_NAMED_SCHEMA_OBJECTS" OF "ORACLE_TOOLS"."T_NAMED_OBJECT" WITH OBJECT IDENTIFIER ("ID") BEQUEATH CURRENT_USER AS
+select  treat(value(t) as oracle_tools.t_named_object)
 from    oracle_tools.v_my_schema_objects t
-where   t.obj is of (oracle_tools.t_named_object);
+where   value(t) is of (oracle_tools.t_named_object);
 
