fb3552ad619ca76fc6aef1c8ffc19f9290c2821e
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-11-15 17:29:44 +0100

    Update V20201115143000__t_text_tab.sql

--- db/app/ddl/src/incr/V20201115143000__t_text_tab.sql
+++ db/app/ddl/src/incr/V20201115143000__t_text_tab.sql
@@ -1,6 +1,6 @@
 -- We must specify an OID to be able to use it remotely.
 begin
-  execute immediate q'[CREATE TYPE "ORACLE_TOOLS"."T_TEXT_TAB" AS TABLE OF VARCHAR2(4000 CHAR)]';
+  execute immediate q'[CREATE TYPE "ORACLE_TOOLS"."T_TEXT_TAB" AS TABLE OF VARCHAR2(4000 BYTE)]';
 end;
 /
 
