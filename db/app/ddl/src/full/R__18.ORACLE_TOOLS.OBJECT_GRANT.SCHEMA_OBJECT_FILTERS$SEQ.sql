64f8f5482333e5e14efcaebe3cfc6ea1afdcbd6d
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-11-30 13:39:23 +0100

    Delete R__18.ORACLE_TOOLS.OBJECT_GRANT.SCHEMA_OBJECT_FILTERS$SEQ.sql

--- db/app/ddl/src/full/R__18.ORACLE_TOOLS.OBJECT_GRANT.SCHEMA_OBJECT_FILTERS$SEQ.sql
+++
@@ -1,4 +0,0 @@
-call dbms_application_info.set_module('R__18.ORACLE_TOOLS.OBJECT_GRANT.SCHEMA_OBJECT_FILTERS$SEQ.sql', null);
-call dbms_application_info.set_action('SQL statement 1');
-GRANT SELECT ON "ORACLE_TOOLS"."SCHEMA_OBJECT_FILTERS$SEQ" TO PUBLIC;
-
