24e8d3bcecb34bddab990552603451da2bda06b4
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-12-01 14:35:25 +0100

    DDL generated

--- db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_DEPENDENT_OR_GRANTED_OBJECT_TYPES.sql
+++ db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_DEPENDENT_OR_GRANTED_OBJECT_TYPES.sql
@@ -1,5 +1,5 @@
-create or replace force view v_dependent_or_granted_object_types as
-select 2 as nr, 'OBJECT_GRANT' as object_type from dual
+CREATE OR REPLACE FORCE VIEW "ORACLE_TOOLS"."V_DEPENDENT_OR_GRANTED_OBJECT_TYPES" ("NR", "OBJECT_TYPE") AS 
+  select 2 as nr, 'OBJECT_GRANT' as object_type from dual
 union all
 select 3, 'SYNONYM' from dual
 union all
@@ -10,3 +10,4 @@ union all
 select 6, 'TRIGGER' from dual
 union all
 select 7, 'INDEX' from dual;
+
--- db/app/ddl/src/full/R__14.ORACLE_TOOLS.PACKAGE_BODY.SCHEMA_OBJECTS_API.sql
+++ db/app/ddl/src/full/R__14.ORACLE_TOOLS.PACKAGE_BODY.SCHEMA_OBJECTS_API.sql
@@ -1200,3 +1200,4 @@ $end
 
 END SCHEMA_OBJECTS_API;
 /
+
