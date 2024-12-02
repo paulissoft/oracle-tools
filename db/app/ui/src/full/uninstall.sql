9a8ae7dc3be30245687f8539672d459f57cfafc1
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-12-02 23:39:02 +0100

    Update uninstall.sql

--- db/app/ui/src/full/uninstall.sql
+++ db/app/ui/src/full/uninstall.sql
@@ -1,7 +1,7 @@
 /* perl generate_ddl.pl (version 2023-01-05) --nodynamic-sql --force-view --group-constraints --skip-install-sql --source-schema=ORACLE_TOOLS --strip-source-schema */
 
 /*
--- JDBC url - username : jdbc:oracle:thin:@bc_dev - BC_PROXY[ORACLE_TOOLS]
+-- JDBC url - username : jdbc:oracle:thin:@//127.0.0.1:1521/freepdb1 - BC_PROXY[ORACLE_TOOLS]
 -- source schema       : 
 -- source database link: 
 -- target schema       : ORACLE_TOOLS
@@ -18,72 +18,92 @@
 */
 -- pkg_ddl_util v4
 call dbms_application_info.set_module('uninstall.sql', null);
-/* SQL statement 1 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_MESSAGES_PKG;;PUBLIC;EXECUTE;NO;2) */
+/* SQL statement 1 (DBMS_SCHEDULER.DROP_JOB;ORACLE_TOOLS;PROCOBJ;UI_RESET_PWD_JOB;;;;;;;;2) */
 call dbms_application_info.set_action('SQL statement 1');
-REVOKE EXECUTE ON "UI_APEX_MESSAGES_PKG" FROM "PUBLIC";
-
-/* SQL statement 2 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_SYNCHRONIZE;;PUBLIC;EXECUTE;NO;2) */
-call dbms_application_info.set_action('SQL statement 2');
-REVOKE EXECUTE ON "UI_APEX_SYNCHRONIZE" FROM "PUBLIC";
-
-/* SQL statement 3 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_ERROR_PKG;;PUBLIC;EXECUTE;NO;2) */
-call dbms_application_info.set_action('SQL statement 3');
-REVOKE EXECUTE ON "UI_ERROR_PKG" FROM "PUBLIC";
-
-/* SQL statement 4 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_SESSION_PKG;;PUBLIC;EXECUTE;NO;2) */
-call dbms_application_info.set_action('SQL statement 4');
-REVOKE EXECUTE ON "UI_SESSION_PKG" FROM "PUBLIC";
-
-/* SQL statement 5 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_USER_MANAGEMENT_PKG;;PUBLIC;EXECUTE;NO;2) */
-call dbms_application_info.set_action('SQL statement 5');
-REVOKE EXECUTE ON "UI_USER_MANAGEMENT_PKG" FROM "PUBLIC";
-
-/* SQL statement 6 (DBMS_SCHEDULER.DROP_JOB;ORACLE_TOOLS;PROCOBJ;UI_RESET_PWD_JOB;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 6');
 BEGIN DBMS_SCHEDULER.DROP_JOB('UI_RESET_PWD_JOB'); END;
 /
 
-/* SQL statement 7 (DROP;ORACLE_TOOLS;PACKAGE_BODY;UI_APEX_MESSAGES_PKG;;;;;;;;2) */
+/* SQL statement 2 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_MESSAGES_PKG;;BC_PORTAL;EXECUTE;NO;2) */
+call dbms_application_info.set_action('SQL statement 2');
+REVOKE EXECUTE ON "UI_APEX_MESSAGES_PKG" FROM "BC_PORTAL";
+
+/* SQL statement 3 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_MESSAGES_PKG;;PUBLIC;EXECUTE;NO;2) */
+call dbms_application_info.set_action('SQL statement 3');
+REVOKE EXECUTE ON "UI_APEX_MESSAGES_PKG" FROM "PUBLIC";
+
+/* SQL statement 4 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_SYNCHRONIZE;;BC_BO;EXECUTE;NO;2) */
+call dbms_application_info.set_action('SQL statement 4');
+REVOKE EXECUTE ON "UI_APEX_SYNCHRONIZE" FROM "BC_BO";
+
+/* SQL statement 5 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_SYNCHRONIZE;;BC_PORTAL;EXECUTE;NO;2) */
+call dbms_application_info.set_action('SQL statement 5');
+REVOKE EXECUTE ON "UI_APEX_SYNCHRONIZE" FROM "BC_PORTAL";
+
+/* SQL statement 6 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_SYNCHRONIZE;;PUBLIC;EXECUTE;NO;2) */
+call dbms_application_info.set_action('SQL statement 6');
+REVOKE EXECUTE ON "UI_APEX_SYNCHRONIZE" FROM "PUBLIC";
+
+/* SQL statement 7 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_ERROR_PKG;;BC_PORTAL;EXECUTE;NO;2) */
 call dbms_application_info.set_action('SQL statement 7');
-DROP PACKAGE BODY UI_APEX_MESSAGES_PKG;
+REVOKE EXECUTE ON "UI_ERROR_PKG" FROM "BC_PORTAL";
 
-/* SQL statement 8 (DROP;ORACLE_TOOLS;PACKAGE_BODY;UI_APEX_SYNCHRONIZE;;;;;;;;2) */
+/* SQL statement 8 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_ERROR_PKG;;PUBLIC;EXECUTE;NO;2) */
 call dbms_application_info.set_action('SQL statement 8');
-DROP PACKAGE BODY UI_APEX_SYNCHRONIZE;
+REVOKE EXECUTE ON "UI_ERROR_PKG" FROM "PUBLIC";
 
-/* SQL statement 9 (DROP;ORACLE_TOOLS;PACKAGE_BODY;UI_ERROR_PKG;;;;;;;;2) */
+/* SQL statement 9 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_SESSION_PKG;;PUBLIC;EXECUTE;NO;2) */
 call dbms_application_info.set_action('SQL statement 9');
-DROP PACKAGE BODY UI_ERROR_PKG;
+REVOKE EXECUTE ON "UI_SESSION_PKG" FROM "PUBLIC";
 
-/* SQL statement 10 (DROP;ORACLE_TOOLS;PACKAGE_BODY;UI_SESSION_PKG;;;;;;;;2) */
+/* SQL statement 10 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_USER_MANAGEMENT_PKG;;PUBLIC;EXECUTE;NO;2) */
 call dbms_application_info.set_action('SQL statement 10');
-DROP PACKAGE BODY UI_SESSION_PKG;
+REVOKE EXECUTE ON "UI_USER_MANAGEMENT_PKG" FROM "PUBLIC";
 
-/* SQL statement 11 (DROP;ORACLE_TOOLS;PACKAGE_BODY;UI_USER_MANAGEMENT_PKG;;;;;;;;2) */
+/* SQL statement 11 (REVOKE;;OBJECT_GRANT;;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_MESSAGES_PKG;;BC_BO;EXECUTE;NO;2) */
 call dbms_application_info.set_action('SQL statement 11');
+REVOKE EXECUTE ON "UI_APEX_MESSAGES_PKG" FROM "BC_BO";
+
+/* SQL statement 12 (DROP;ORACLE_TOOLS;PACKAGE_BODY;UI_USER_MANAGEMENT_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 12');
 DROP PACKAGE BODY UI_USER_MANAGEMENT_PKG;
 
-/* SQL statement 12 (DROP;ORACLE_TOOLS;VIEW;UI_APEX_MESSAGES_V;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 12');
+/* SQL statement 13 (DROP;ORACLE_TOOLS;PACKAGE_BODY;UI_SESSION_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 13');
+DROP PACKAGE BODY UI_SESSION_PKG;
+
+/* SQL statement 14 (DROP;ORACLE_TOOLS;PACKAGE_BODY;UI_ERROR_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 14');
+DROP PACKAGE BODY UI_ERROR_PKG;
+
+/* SQL statement 15 (DROP;ORACLE_TOOLS;PACKAGE_BODY;UI_APEX_SYNCHRONIZE;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 15');
+DROP PACKAGE BODY UI_APEX_SYNCHRONIZE;
+
+/* SQL statement 16 (DROP;ORACLE_TOOLS;PACKAGE_BODY;UI_APEX_MESSAGES_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 16');
+DROP PACKAGE BODY UI_APEX_MESSAGES_PKG;
+
+/* SQL statement 17 (DROP;ORACLE_TOOLS;VIEW;UI_APEX_MESSAGES_V;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 17');
 DROP VIEW UI_APEX_MESSAGES_V;
 
-/* SQL statement 13 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_MESSAGES_PKG;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 13');
-DROP PACKAGE UI_APEX_MESSAGES_PKG;
+/* SQL statement 18 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;UI_USER_MANAGEMENT_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 18');
+DROP PACKAGE UI_USER_MANAGEMENT_PKG;
 
-/* SQL statement 14 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_SYNCHRONIZE;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 14');
-DROP PACKAGE UI_APEX_SYNCHRONIZE;
-
-/* SQL statement 15 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;UI_ERROR_PKG;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 15');
+/* SQL statement 19 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;UI_ERROR_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 19');
 DROP PACKAGE UI_ERROR_PKG;
 
-/* SQL statement 16 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;UI_SESSION_PKG;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 16');
+/* SQL statement 20 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_SYNCHRONIZE;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 20');
+DROP PACKAGE UI_APEX_SYNCHRONIZE;
+
+/* SQL statement 21 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;UI_SESSION_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 21');
 DROP PACKAGE UI_SESSION_PKG;
 
-/* SQL statement 17 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;UI_USER_MANAGEMENT_PKG;;;;;;;;2) */
-call dbms_application_info.set_action('SQL statement 17');
-DROP PACKAGE UI_USER_MANAGEMENT_PKG;
+/* SQL statement 22 (DROP;ORACLE_TOOLS;PACKAGE_SPEC;UI_APEX_MESSAGES_PKG;;;;;;;;2) */
+call dbms_application_info.set_action('SQL statement 22');
+DROP PACKAGE UI_APEX_MESSAGES_PKG;
 
