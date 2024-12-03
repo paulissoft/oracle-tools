4ca2258f754b81a5dcbebbc928a034d24d789f1a
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-12-03 10:41:08 +0100

    DDL generated.

--- db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_MY_GENERATE_DDL_SESSIONS.sql
+++ db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_MY_GENERATE_DDL_SESSIONS.sql
@@ -1,5 +1,5 @@
-create or replace view v_my_generate_ddl_sessions as
-select  gds.session_id
+CREATE OR REPLACE VIEW "ORACLE_TOOLS"."V_MY_GENERATE_DDL_SESSIONS" ("SESSION_ID", "GENERATE_DDL_CONFIGURATION_ID", "SCHEMA_OBJECT_FILTER_ID", "CREATED", "USERNAME", "UPDATED") AS 
+  select  gds.session_id
 ,       gds.generate_ddl_configuration_id
 ,       gds.schema_object_filter_id
 ,       gds.created
@@ -7,3 +7,4 @@ select  gds.session_id
 ,       gds.updated
 from    oracle_tools.generate_ddl_sessions gds
 where   gds.username = user;
+
