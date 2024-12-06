4a33e952741b9fbe3f4a1a56fd4a2db8b4462ad1
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-12-06 12:29:02 +0100

    DDL generated.

--- db/app/ddl/src/full/02.ORACLE_TOOLS.TYPE_SPEC.T_DISPLAY_DDL_SQL.sql
+++ db/app/ddl/src/full/02.ORACLE_TOOLS.TYPE_SPEC.T_DISPLAY_DDL_SQL.sql
@@ -5,6 +5,9 @@ CREATE TYPE "ORACLE_TOOLS"."T_DISPLAY_DDL_SQL" authid definer as object
 , ddl_info varchar2(1000 byte)
 , chunk# integer
 , chunk varchar2(4000 byte)
+  -- attributes below are only set for the last chunk
+, last_chunk number(1, 0) -- 1 or null
+, schema_object oracle_tools.t_schema_object -- not null if and only if last_chunk = 1
 )
 instantiable
 final;
