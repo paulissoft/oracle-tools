9f2917a4ab255edafcd813181ffb23a2a60e5088
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-12-08 17:50:36 +0100

    Update R__10.ORACLE_TOOLS.VIEW.V_MY_SCHEMA_OBJECT_FILTER.sql

--- db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_MY_SCHEMA_OBJECT_FILTER.sql
+++ db/app/ddl/src/full/R__10.ORACLE_TOOLS.VIEW.V_MY_SCHEMA_OBJECT_FILTER.sql
@@ -1,12 +1,17 @@
 CREATE OR REPLACE VIEW "ORACLE_TOOLS"."V_MY_SCHEMA_OBJECT_FILTER" ("SESSION_ID", "SCHEMA_OBJECT_FILTER_ID", "OBJ_JSON", "SCHEMA", "GRANTOR_IS_SCHEMA") BEQUEATH CURRENT_USER AS 
-  select  /* V_MY_SCHEMA_OBJECT_FILTER since there is just one current filter */ gds.session_id
+  with sof as
+( select  sof.id
+  ,       sof.obj_json
+  ,       treat(oracle_tools.t_object_json.deserialize('ORACLE_TOOLS.T_SCHEMA_OBJECT_FILTER', sof.obj_json) as oracle_tools.t_schema_object_filter) as obj
+  from    oracle_tools.schema_object_filters sof
+)
+select  /* V_MY_SCHEMA_OBJECT_FILTER since there is just one current filter */ gds.session_id
 ,       gds.schema_object_filter_id
 ,       sof.obj_json
---,       treat(oracle_tools.t_object_json.deserialize('ORACLE_TOOLS.T_SCHEMA_OBJECT_FILTER', sof.obj) as oracle_tools.t_schema_object_filter) as obj
-,       sof.obj_json.schema$ as schema
-,       sof.obj_json.grantor_is_schema$ as grantor_is_schema
+,       sof.obj.schema$ as schema
+,       sof.obj.grantor_is_schema$ as grantor_is_schema
 from    oracle_tools.generate_ddl_sessions gds
-        inner join oracle_tools.schema_object_filters sof
+        inner join sof
         on sof.id = gds.schema_object_filter_id
 where   gds.session_id = (select oracle_tools.ddl_crud_api.get_session_id from dual where rownum = 1);
 
