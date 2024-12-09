ff49a27cbd2ac8965d5da6037e7fdd053ed7a06a
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-12-09 22:36:43 +0100

    DDL generated.

--- db/app/ddl/src/full/R__18.ORACLE_TOOLS.OBJECT_GRANT.P_GENERATE_DDL.sql
+++ db/app/ddl/src/full/R__18.ORACLE_TOOLS.OBJECT_GRANT.P_GENERATE_DDL.sql
@@ -1,4 +1,7 @@
 call dbms_application_info.set_module('R__18.ORACLE_TOOLS.OBJECT_GRANT.P_GENERATE_DDL.sql', null);
 call dbms_application_info.set_action('SQL statement 1');
+GRANT EXECUTE ON "ORACLE_TOOLS"."P_GENERATE_DDL" TO "ADMIN" WITH GRANT OPTION;
+
+call dbms_application_info.set_action('SQL statement 2');
 GRANT EXECUTE ON "ORACLE_TOOLS"."P_GENERATE_DDL" TO PUBLIC;
 
