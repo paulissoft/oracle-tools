598e5251ae48110ca33526cde565355a381935ea
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2024-12-10 19:37:39 +0100

    Update R__10.02.ORACLE_TOOLS.VIEW.V_MY_NAMED_SCHEMA_OBJECTS.sql

--- db/app/ddl/src/full/R__10.02.ORACLE_TOOLS.VIEW.V_MY_NAMED_SCHEMA_OBJECTS.sql
+++ db/app/ddl/src/full/R__10.02.ORACLE_TOOLS.VIEW.V_MY_NAMED_SCHEMA_OBJECTS.sql
@@ -1,14 +1,18 @@
 CREATE OR REPLACE VIEW "ORACLE_TOOLS"."V_MY_NAMED_SCHEMA_OBJECTS" OF "ORACLE_TOOLS"."T_NAMED_OBJECT"
-  WITH OBJECT IDENTIFIER ("ID") BEQUEATH CURRENT_USER  AS 
-  select  treat(vso.obj as oracle_tools.t_named_object)
-from    oracle_tools.v_schema_objects vso
-where   vso.session_id =
+  WITH OBJECT IDENTIFIER ("ID") BEQUEATH CURRENT_USER  AS
+  select  treat(so.obj as oracle_tools.t_named_object)
+from    oracle_tools.generate_ddl_sessions gds
+        inner join oracle_tools.schema_object_filter_results sofr
+        on sofr.schema_object_filter_id = gds.schema_object_filter_id
+    	inner join oracle_tools.schema_objects so
+	    on so.id = sofr.schema_object_id
+where   gds.session_id =
         -- use old trick to invoke get_session_id just once
         (select oracle_tools.ddl_crud_api.get_session_id from dual where rownum <= 1)
-and     vso.generate_ddl is not null
-and     vso.obj is of (oracle_tools.t_named_object)
+and     sofr.generate_ddl is not null
+and     so.obj is of (oracle_tools.t_named_object)
 order by
         -- order of creation
-        vso.session_id
-,       vso.created;
+        gds.session_id
+,       so.created;
 
