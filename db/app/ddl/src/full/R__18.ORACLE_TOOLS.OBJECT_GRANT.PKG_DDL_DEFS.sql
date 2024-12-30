fc0a10d7d8b4f2acb09ca6ba67d1852ad69359a2
Gert-Jan Paulissen <paulissoft@gmail.com>
2024-12-30 10:17:44 +0100

    Release 4.1.1

---
+++ db/app/ddl/src/full/R__18.ORACLE_TOOLS.OBJECT_GRANT.PKG_DDL_DEFS.sql
@@ -0,0 +1,4 @@
+call dbms_application_info.set_module('R__18.ORACLE_TOOLS.OBJECT_GRANT.PKG_DDL_DEFS.sql', null);
+call dbms_application_info.set_action('SQL statement 1');
+GRANT EXECUTE ON "ORACLE_TOOLS"."PKG_DDL_DEFS" TO PUBLIC;
+
