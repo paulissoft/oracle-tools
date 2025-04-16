4fd941bb01f091c10b42f50e9ad265a26ea41402
Gert-Jan Paulissen <paulissoft@gmail.com>
2025-04-16 14:54:00 +0200

    Update export_plsql.sh

--- apex/src/scripts/export_plsql.sh
+++ apex/src/scripts/export_plsql.sh
@@ -5,8 +5,7 @@ set -x
 declare -r srcdir=$(cd $(dirname $0) && pwd)
 
 declare -r sql=$1
-declare -r userid=$2
-declare -r workspace_name=$3
-declare -r application=$4
+declare -r workspace_name=$2
+declare -r application=$3
 
-$sql -S "$userid" @${srcdir}/export_plsql.sql "$workspace_name" "$application" | perl $srcdir/export_plsql.pl
+$sql -S @${srcdir}/export_plsql.sql "$workspace_name" "$application" <<< "$USERID" | perl $srcdir/export_plsql.pl
