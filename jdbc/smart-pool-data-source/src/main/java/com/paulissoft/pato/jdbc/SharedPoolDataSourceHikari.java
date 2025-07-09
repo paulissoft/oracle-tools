07855d4d09e4b0a1e7db91572f4d3f793e5ff664
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2025-07-09 13:23:57 +0200

    Update SharedPoolDataSourceHikari.java

--- jdbc/smart-pool-data-source/src/main/java/com/paulissoft/pato/jdbc/SharedPoolDataSourceHikari.java
+++ jdbc/smart-pool-data-source/src/main/java/com/paulissoft/pato/jdbc/SharedPoolDataSourceHikari.java
@@ -17,7 +17,9 @@ import java.util.concurrent.ThreadFactory;
 import java.util.logging.Logger;
 import javax.sql.DataSource;
 import java.util.function.Function;
-    
+import java.util.stream.Collectors;
+import java.util.Objects;
+
 
 // a package accessible class
 class SharedPoolDataSourceHikari {
@@ -42,11 +44,31 @@ class SharedPoolDataSourceHikari {
         ds.setMaximumPoolSize(members.stream().mapToInt(HikariDataSource::getMaximumPoolSize).sum());
 
         // properties that may NOT differ, i.e. must be common
-        /*
+
         // private String dataSourceClassName;
-        // 
+        var streamDataSourceClassName = members.stream().map(HikariDataSource::getDataSourceClassName);
+        
+        if (streamDataSourceClassName.filter(Objects::isNull).count() == members.size()) {
+            /* all null */
+            ds.setDataSourceClassName(null);
+        } else if (streamDataSourceClassName.filter(Objects::nonNull).count() == members.size() &&
+                   streamDataSourceClassName.filter(Objects::nonNull).distinct().count() == 1) {
+            /* all not null and the same */
+            ds.setDataSourceClassName(members.get(0).getDataSourceClassName());
+        } else {
+            throw new IllegalStateException(String.format("Not all data source class names are the same: %s", streamDataSourceClassName.collect(Collectors.toList()).toString()));
+        }
+
         // private boolean autoCommit;
-        // 
+        var streamAutoCommit = members.stream().map(HikariDataSource::isAutoCommit);
+
+        if (streamAutoCommit.distinct().count() == 1) {
+            /* all the same */
+            ds.setAutoCommit(members.get(0).isAutoCommit());
+        } else {
+            throw new IllegalStateException(String.format("Not all auto commit values are the same: %s", streamAutoCommit.collect(Collectors.toList()).toString()));
+        }
+        
         // private long connectionTimeout;
         // 
         // private long idleTimeout;
@@ -68,7 +90,7 @@ class SharedPoolDataSourceHikari {
         // private long validationTimeout;
         // 
         // private long leakDetectionThreshold;
-        */
+        //
     }
 
     public Connection getConnection() throws SQLException {
