2db03114b1a2315713d13010005570514e38246f
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2025-07-10 09:10:05 +0200

    Update SharedPoolDataSourceHikari.java

--- jdbc/smart-pool-data-source/src/main/java/com/paulissoft/pato/jdbc/SharedPoolDataSourceHikari.java
+++ jdbc/smart-pool-data-source/src/main/java/com/paulissoft/pato/jdbc/SharedPoolDataSourceHikari.java
@@ -73,90 +73,71 @@ class SharedPoolDataSourceHikari extends SharedPoolDataSource<HikariDataSource> 
         ds.setMinimumIdle(members.stream().mapToInt(HikariDataSource::getMinimumIdle).sum());
         ds.setMaximumPoolSize(members.stream().mapToInt(HikariDataSource::getMaximumPoolSize).sum());
 
-        // Must use lambda expressions below since primitives are used and
-        // the functional interface does not support them all (boolean).
+        // Must use lambda expressions for methods with primitive arguments or return values
+	// since the functional interface does not support them all (boolean).
+	// But auto-boxing handles this implicitly.
         
         // properties that may NOT differ, i.e. must be common
 
-        // private String username;
         // just a check: no need to invoke ds.setUsername() since that has been done already in SmartPoolDataSourceHikari
         checkStringProperty(HikariDataSource::getUsername, "username");
 
-        // private String catalog;
         configureStringProperty(HikariDataSource::getCatalog, HikariDataSource::setCatalog, "catalog");
 
-        // private String connectionInitSql;
         configureStringProperty(HikariDataSource::getConnectionInitSql, HikariDataSource::setConnectionInitSql, "connection init sql");
 
-        // private String dataSourceClassName;
         configureStringProperty(HikariDataSource::getDataSourceClassName, HikariDataSource::setDataSourceClassName, "data source class name");
 
-        // private String dataSourceJNDI;
         configureStringProperty(HikariDataSource::getDataSourceJNDI, HikariDataSource::setDataSourceJNDI, "data source JNDI");
 
-        // private String driverClassName;
         configureStringProperty(HikariDataSource::getDriverClassName, HikariDataSource::setDriverClassName, "driver class name");
 
-        // private boolean allowPoolSuspension;
         configureBooleanProperty((ds) -> ds.isAllowPoolSuspension(),
                                  (ds, value) -> ds.setAllowPoolSuspension(value),
                                  "allow pool suspension");
 
-        // private boolean autoCommit;
         configureBooleanProperty((ds) -> ds.isAutoCommit(),
                                  (ds, value) -> ds.setAutoCommit(value),
                                  "auto commit");
         
-        // private long connectionTimeout;
         configureLongProperty((ds) -> ds.getConnectionTimeout(),
                               (ds, value) -> ds.setConnectionTimeout(value),
                               "connection timeout");
 
-        // private long idleTimeout;
         configureLongProperty((ds) -> ds.getIdleTimeout(),
                               (ds, value) -> ds.setIdleTimeout(value),
                               "idle timeout");
 
-        // private long initializationFailTimeout;
         configureLongProperty((ds) -> ds.getInitializationFailTimeout(),
                               (ds, value) -> ds.setInitializationFailTimeout(value),
                               "initialization fail timeout");
 
-        // private String jdbcUrl;
         configureStringProperty(HikariDataSource::getJdbcUrl, HikariDataSource::setJdbcUrl, "JDBC URL");
 
-        // private long maxLifetime;
         configureLongProperty((ds) -> ds.getMaxLifetime(),
                               (ds, value) -> ds.setMaxLifetime(value),
                               "max lifetime");
 
-        // private boolean isolateInternalQueries;
         configureBooleanProperty((ds) -> ds.isIsolateInternalQueries(),
                                  (ds, value) -> ds.setIsolateInternalQueries(value),
                                  "isolate internal queries");
 
-        // private boolean readOnly;
         configureBooleanProperty((ds) -> ds.isReadOnly(),
                                  (ds, value) -> ds.setReadOnly(value),
                                  "read only");
 
-        // private boolean registerMbeans;
         configureBooleanProperty((ds) -> ds.isRegisterMbeans(),
                                  (ds, value) -> ds.setRegisterMbeans(value),
                                  "register Mbeans");
 
-        // private String schema;
         configureStringProperty(HikariDataSource::getSchema, HikariDataSource::setSchema, "schema");
 
-        // private String transactionIsolation;
         configureStringProperty(HikariDataSource::getTransactionIsolation, HikariDataSource::setTransactionIsolation, "transaction isolation");
 
-        // private long validationTimeout;
         configureLongProperty((ds) -> ds.getValidationTimeout(),
                               (ds, value) -> ds.setValidationTimeout(value),
                               "validation timeout");
 
-        // private long leakDetectionThreshold;
         configureLongProperty((ds) -> ds.getLeakDetectionThreshold(),
                               (ds, value) -> ds.setLeakDetectionThreshold(value),
                               "leak detection threshold");
