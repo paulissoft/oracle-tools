a6896b353e8613d1b8b0a6b1eafe0598859f79e1
Gert-Jan Paulissen <paulissoft@gmail.com>
2025-08-10 11:38:22 +0200

    WEB_SERVICE_RESPONSE_TYP OK

--- db/app/msg/src/full/R__15.TYPE_BODY.WEB_SERVICE_RESPONSE_TYP.sql
+++ db/app/msg/src/full/R__15.TYPE_BODY.WEB_SERVICE_RESPONSE_TYP.sql
@@ -2,17 +2,20 @@ CREATE OR REPLACE TYPE BODY "WEB_SERVICE_RESPONSE_TYP" AS
 
 constructor function web_service_response_typ
 ( self in out nocopy web_service_response_typ
-, p_group$ in varchar2
-, p_context$ in varchar2
+  -- from MSG_TYP
+, p_group$ in varchar2 default null                -- use default_group() from below
+, p_context$ in varchar2 default null
+  -- from HTTP_REQUEST_RESPONSE_TYP
+, p_cookies in http_cookie_tab_typ default null    -- request/response cookies
+, p_http_headers in property_tab_typ default null  -- request/response headers
+, p_body_clob in clob default null                 -- empty for GET request (envelope for a SOAP request)
+, p_body_blob in blob default null                 -- empty for GET request (empty for a SOAP request)
+  -- from WEB_SERVICE_RESPONSE_TYP
 , p_web_service_request in web_service_request_typ
 , p_sql_code in integer
 , p_sql_error_message in varchar2
 , p_http_status_code in integer  
-, p_body_clob in clob
-, p_body_blob in blob
-, p_cookies_clob in clob
-, p_http_headers_clob in clob
-, p_http_reason_phrase in varchar2
+, p_http_reason_phrase in varchar2 default null
 )
 return self as result
 is
@@ -20,14 +23,14 @@ begin
   self.construct
   ( p_group$ => p_group$
   , p_context$ => p_context$
+  , p_cookies => p_cookies
+  , p_http_headers => p_http_headers
+  , p_body_clob => p_body_clob
+  , p_body_blob => p_body_blob
   , p_web_service_request => p_web_service_request
   , p_sql_code => p_sql_code
   , p_sql_error_message => p_sql_error_message
   , p_http_status_code => p_http_status_code
-  , p_body_clob => p_body_clob
-  , p_body_blob => p_body_blob
-  , p_cookies_clob => p_cookies_clob
-  , p_http_headers_clob => p_http_headers_clob
   , p_http_reason_phrase => p_http_reason_phrase
   );
   return;
@@ -42,14 +45,14 @@ begin
   self.construct
   ( p_group$ => null
   , p_context$ => null
+  , p_cookies => null
+  , p_http_headers => null
+  , p_body_clob => null
+  , p_body_blob => null
   , p_web_service_request => null
   , p_sql_code => null
   , p_sql_error_message => null
   , p_http_status_code => null
-  , p_body_clob => null
-  , p_body_blob => null
-  , p_cookies_clob => null
-  , p_http_headers_clob => null
   , p_http_reason_phrase => null
   );
   return;
@@ -57,29 +60,35 @@ end web_service_response_typ;
 
 final member procedure construct
 ( self in out nocopy web_service_response_typ
+  -- from MSG_TYP
 , p_group$ in varchar2
 , p_context$ in varchar2
+  -- from HTTP_REQUEST_RESPONSE_TYP
+, p_cookies in http_cookie_tab_typ
+, p_http_headers in property_tab_typ
+, p_body_clob in clob
+, p_body_blob in blob
+  -- from WEB_SERVICE_RESPONSE_TYP
 , p_web_service_request in web_service_request_typ
 , p_sql_code in integer
 , p_sql_error_message in varchar2
 , p_http_status_code in integer  
-, p_body_clob in clob
-, p_body_blob in blob
-, p_cookies_clob in clob
-, p_http_headers_clob in clob
 , p_http_reason_phrase in varchar2
 )
 is
 begin
-  (self as msg_typ).construct(nvl(p_group$, web_service_response_typ.default_group()), p_context$);
+  (self as http_request_response_typ).construct
+  ( p_group$ => nvl(p_group$, web_service_response_typ.default_group())
+  , p_context$ => p_context$
+  , p_cookies => p_cookies
+  , p_http_headers => p_http_headers
+  , p_body_clob => p_body_clob
+  , p_body_blob => p_body_blob  
+  );
   self.web_service_request := p_web_service_request;
   self.sql_code := p_sql_code;
   self.sql_error_message := p_sql_error_message;
   self.http_status_code := p_http_status_code;
-  msg_pkg.data2msg(p_body_clob, self.body_vc, self.body_clob);
-  msg_pkg.data2msg(p_body_blob, self.body_raw, self.body_blob);
-  msg_pkg.data2msg(p_cookies_clob, self.cookies_vc, self.cookies_clob);
-  msg_pkg.data2msg(p_http_headers_clob, self.http_headers_vc, self.http_headers_clob);
   self.http_reason_phrase := p_http_reason_phrase;
 end construct;
 
@@ -165,32 +174,8 @@ member procedure serialize
 )
 is
   l_web_service_request json_object_t := json_object_t();
-  l_cookies_vc constant json_array_t := 
-    case
-      when self.cookies_vc is not null
-      then json_array_t(self.cookies_vc)
-      else null
-    end;
-  l_cookies_clob constant json_array_t := 
-    case
-      when self.cookies_clob is not null
-      then json_array_t(self.cookies_clob)
-      else null
-    end;
-  l_http_headers_vc constant json_array_t := 
-    case
-      when self.http_headers_vc is not null
-      then json_array_t(self.http_headers_vc)
-      else null
-    end;
-  l_http_headers_clob constant json_array_t := 
-    case
-      when self.http_headers_clob is not null
-      then json_array_t(self.http_headers_clob)
-      else null
-    end;
 begin
-  (self as msg_typ).serialize(p_json_object);
+  (self as http_request_response_typ).serialize(p_json_object);
   if self.web_service_request is not null
   then
     self.web_service_request.serialize(l_web_service_request);
@@ -199,64 +184,8 @@ begin
   p_json_object.put('SQL_CODE', self.sql_code);
   p_json_object.put('SQL_ERROR_MESSAGE', self.sql_error_message);
   p_json_object.put('HTTP_STATUS_CODE', self.http_status_code);
-  if self.body_vc is not null
-  then
-    p_json_object.put('BODY_VC', self.body_vc);
-  end if;
-  if self.body_clob is not null
-  then
-    p_json_object.put('BODY_CLOB', self.body_clob);
-  end if;
-  if self.body_raw is not null
-  then
-    p_json_object.put('BODY_RAW', self.body_raw);
-  end if;
-  if self.body_blob is not null
-  then
-    p_json_object.put('BODY_BLOB', self.body_blob);
-  end if;
-  if l_cookies_vc is not null
-  then
-    p_json_object.put('COOKIES_VC', l_cookies_vc);
-  end if;
-  if l_cookies_clob is not null
-  then
-    p_json_object.put('COOKIES_CLOB', l_cookies_clob);
-  end if;
-  if l_http_headers_vc is not null
-  then
-    p_json_object.put('HTTP_HEADERS_VC', l_http_headers_vc);
-  end if;
-  if l_http_headers_clob is not null
-  then
-    p_json_object.put('HTTP_HEADERS_CLOB', l_http_headers_clob);
-  end if;
   p_json_object.put('HTTP_REASON_PHRASE', self.http_reason_phrase);
 end serialize;
-
-overriding
-member function has_not_null_lob
-( self in web_service_response_typ
-)
-return integer
-is
-begin
-  return
-    case
-      when self.web_service_request is not null and
-           self.web_service_request.has_not_null_lob = 1
-      then 1
-      when self.body_clob is not null
-      then 1
-      when self.body_blob is not null
-      then 1
-      when self.cookies_clob is not null
-      then 1
-      when self.http_headers_clob is not null
-      then 1
-      else 0
-    end;
-end has_not_null_lob;
 
 overriding
 member function default_processing_method
