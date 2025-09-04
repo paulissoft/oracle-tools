378c51d14d3cc360cdef9ea8ef110649cfac8e36
Gert-Jan Paulissen <paulissoft@gmail.com>
2025-09-04 18:13:46 +0200

    Update purge_flyway_table.sql

--- db/src/scripts/purge_flyway_table.sql
+++ db/src/scripts/purge_flyway_table.sql
@@ -3,6 +3,11 @@ set define on termout on verify off feedback off
 define flyway_table = "&1"
 define nr_months_to_keep_flyway_table = "&2"
 
+/*
+Bootstrap problem: SQL Error: ORA-00904: "ORACLE_TOOLS"."CFG_INSTALL_PKG"."PURGE_FLYWAY_TABLE": invalid identifier
+*/
+whenever sqlerror exit success
+
 select oracle_tools.cfg_install_pkg.purge_flyway_table('"&flyway_table"', &nr_months_to_keep_flyway_table) as "# rows purged from &_USER..&flyway_table" from dual;
 
 exit sql.sqlcode
