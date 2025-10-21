8cf2f9dbe81da7a5881476364c01ea297fb060c4
Gert-Jan Paulissen <paulissoft@gmail.com>
2025-10-21 10:39:00 +0200

    Adding GITHUB installation tables.

--- db/app/admin/adb-install-bootstrap.sql
+++ db/app/admin/adb-install-bootstrap.sql
@@ -9,6 +9,13 @@ whenever sqlerror exit failure
 
 alter session set current_schema = ADMIN;
 
+prompt @@src/incr/V20251021102600__create_GITHUB_INSTALLED_PROJECTS.sql
+@@src/incr/V20251021102600__create_GITHUB_INSTALLED_PROJECTS.sql
+prompt @@src/incr/V20251021103000__create_GITHUB_INSTALLED_VERSIONS.sql
+@@src/incr/V20251021103000__create_GITHUB_INSTALLED_VERSIONS.sql
+prompt @@src/incr/V20251021103400__create_GITHUB_INSTALLED_VERSIONS_OBJECTS.sql
+@@src/incr/V20251021103400__create_GITHUB_INSTALLED_VERSIONS_OBJECTS.sql
+
 prompt @@src/full/R__09.PACKAGE_SPEC.ADMIN_INSTALL_PKG.sql
 @@src/full/R__09.PACKAGE_SPEC.ADMIN_INSTALL_PKG.sql
 prompt @@src/full/R__14.PACKAGE_BODY.ADMIN_INSTALL_PKG.sql
---
+++ db/app/admin/src/incr/V20251021103400__create_GITHUB_INSTALLED_VERSIONS_OBJECTS.sql
@@ -0,0 +1,63 @@
+declare
+begin
+  execute immediate q'<
+CREATE TABLE "GITHUB_INSTALLED_VERSIONS_OBJECTS" (
+    "GITHUB_INSTALLED_VERSIONS_ID" INTEGER
+        CONSTRAINT "NNC_GITHUB_INSTALLED_VERSIONS_OBJECTS_GITHUB_INSTALLED_VERSIONS_ID" NOT NULL,
+    "OBJECT_TYPE"                  VARCHAR2(23)
+        CONSTRAINT "NNC_GITHUB_INSTALLED_VERSIONS_OBJECTS_OBJECT_TYPE" NOT NULL,
+    "OBJECT_NAME"                  VARCHAR2(128)
+        CONSTRAINT "NNC_GITHUB_INSTALLED_VERSIONS_OBJECTS_OBJECT_NAME" NOT NULL,
+    "LAST_DDL_TIME"                DATE
+        CONSTRAINT "NNC_GITHUB_INSTALLED_VERSIONS_OBJECTS_LAST_DDL_TIME" NOT NULL
+)
+LOGGING
+>';
+
+  execute immediate q'<
+COMMENT ON TABLE "GITHUB_INSTALLED_VERSIONS_OBJECTS" IS
+    'GitHub installed versions objects. Used to determine whether at least one of the objects has changed since the installation date.'
+>';
+    
+  execute immediate q'<
+COMMENT ON COLUMN "GITHUB_INSTALLED_VERSIONS_OBJECTS"."OBJECT_TYPE" IS
+    'ALL_OBJECTS.OBJECT_TYPE'
+>';
+   
+  execute immediate q'<
+COMMENT ON COLUMN "GITHUB_INSTALLED_VERSIONS_OBJECTS"."OBJECT_NAME" IS
+    'ALL_OBJECTS.OBJECT_NAME'
+>';
+
+  execute immediate q'<
+COMMENT ON COLUMN "GITHUB_INSTALLED_VERSIONS_OBJECTS"."LAST_DDL_TIME" IS
+    'ALL_OBJECTS.LAST_DDL_TIME'
+>';
+
+  execute immediate q'<
+ALTER TABLE "GITHUB_INSTALLED_VERSIONS_OBJECTS"
+    ADD CONSTRAINT "GITHUB_INSTALLED_VERSIONS_OBJECTS_PK" PRIMARY KEY ( "GITHUB_INSTALLED_VERSIONS_ID",
+                                                                        "OBJECT_TYPE",
+                                                                        "OBJECT_NAME" )
+>';
+                                                                       
+  execute immediate q'<
+ALTER TABLE "GITHUB_INSTALLED_VERSIONS_OBJECTS"
+    ADD CONSTRAINT "GITHUB_INSTALLED_VERSIONS_FK" FOREIGN KEY ( "GITHUB_INSTALLED_VERSIONS_ID" )
+        REFERENCES "ADMIN"."GITHUB_INSTALLED_VERSIONS" ( "ID" )
+    NOT DEFERRABLE
+>';
+
+  execute immediate q'<
+CREATE TRIGGER "FKNTM_GITHUB_INSTALLED_VERSIONS_OBJECTS" BEFORE
+    UPDATE OF "GITHUB_INSTALLED_VERSIONS_ID" ON "GITHUB_INSTALLED_VERSIONS_OBJECTS"
+BEGIN
+    raise_application_error(-20225, 'Non Transferable FK constraint  on table "GITHUB_INSTALLED_VERSIONS_OBJECTS" is violated');
+END;
+>';
+
+exception
+  when others
+  then raise;
+end;
+/
