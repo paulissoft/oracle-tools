83e99139b3b5b34ed8cdf04c4d706482454b00cf
Gert-Jan Paulissen <paulissoft@gmail.com>
2025-10-21 16:02:10 +0200

    Update V20241101115701__schema_objects.sql

--- db/app/ddl/src/incr/V20241101115701__schema_objects.sql
+++ db/app/ddl/src/incr/V20241101115701__schema_objects.sql
@@ -1,3 +1,5 @@
+begin
+  execute immediate q'<
 create table schema_objects
 ( id varchar2(500 byte)
   constraint schema_objects$nnc$id not null
@@ -9,17 +11,26 @@ create table schema_objects
 , constraint schema_objects$pk primary key (id)
 , constraint schema_objects$ck$obj check (obj is not null and obj.id = id)
 , constraint schema_objects$ck$obj$last_ddl_time$ check (obj.last_ddl_time$ is not null) deferrable initially deferred
-);
+)
+>';
 
-alter table schema_objects nologging;
+  execute immediate q'<
+alter table schema_objects nologging
+>';
 
+  execute immediate q'<
 create index schema_objects$idx$1
-on schema_objects (obj.object_schema(), obj.object_name(), obj.object_type());
+on schema_objects (obj.object_schema(), obj.object_name(), obj.object_type())
+>';
 
+  execute immediate q'<
 create index schema_objects$idx$2
-on schema_objects (obj.base_object_schema(), obj.base_object_name(), obj.base_object_type());
+on schema_objects (obj.base_object_schema(), obj.base_object_name(), obj.base_object_type())
+>';
 
+  execute immediate q'<
 comment on table schema_objects is
-    'The schema objects including named object and dependent or granted objects.';
-
-
+    'The schema objects including named object and dependent or granted objects.'
+>';
+end;
+/
