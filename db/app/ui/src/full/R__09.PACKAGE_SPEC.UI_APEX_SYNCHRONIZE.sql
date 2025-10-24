fdd5ac4d25ac16043a4699d7ad442f0f67dd5df3
Gert-Jan Paulissen <paulissoft@gmail.com>
2025-10-24 10:11:16 +0200

    Update R__09.PACKAGE_SPEC.UI_APEX_SYNCHRONIZE.sql

--- db/app/ui/src/full/R__09.PACKAGE_SPEC.UI_APEX_SYNCHRONIZE.sql
+++ db/app/ui/src/full/R__09.PACKAGE_SPEC.UI_APEX_SYNCHRONIZE.sql
@@ -14,7 +14,7 @@ procedure pre_import
 procedure prepare_import
 ( p_workspace_name in apex_workspaces.workspace%type
 , p_application_id in apex_application_trans_map.primary_application_id%type
-, p_user in varchar2 default user
+, p_user in varchar2 default sys_context('USERENV', 'CURRENT_SCHEMA')
 );
 
 /**
